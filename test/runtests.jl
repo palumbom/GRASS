94ea4b5b38838b40bc45885064dbb535f1cc496d
Michael Palumbo <palumbomi95@gmail.com>
2021-05-05 14:08:49 -0400

    fixed old package name in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+using Pkg; Pkg.activate(".")
 using GRASS
 using Test
 
--- test/test_geometry.jl
+++ test/test_geometry.jl
@@ -3,34 +3,34 @@ using Test
 @testset "Geometry" begin
 
 @testset "Testing function definitions" begin
-    @test isdefined(SS, :make_grid)
-    @test isdefined(SS, :calc_r2)
-    @test isdefined(SS, :calc_mu)
-    @test isdefined(SS, :find_nearest_mu)
-    @test isdefined(SS, :find_nearest_ax)
+    @test isdefined(GRASS, :make_grid)
+    @test isdefined(GRASS, :calc_r2)
+    @test isdefined(GRASS, :calc_mu)
+    @test isdefined(GRASS, :find_nearest_mu)
+    @test isdefined(GRASS, :find_nearest_ax)
 end
 
 @testset "Testing back/forth conversions" begin
     t = (0.75, 0.5)
-    @test (0.75^2 + 0.5^2) == SS.calc_r2(t)
-    @test sqrt(1.0 - SS.calc_r2(t)) == SS.calc_mu(t)
-    @test SS.make_grid(256) == SS.make_grid(N=256)
-    @test SS.calc_mu(t) == SS.calc_mu(t[1], t[2])
-    @test SS.calc_mu((0.0, 0.0)) == 1.0
-    @test SS.calc_mu((1.0, 2.5)) == 0.0
+    @test (0.75^2 + 0.5^2) == GRASS.calc_r2(t)
+    @test sqrt(1.0 - GRASS.calc_r2(t)) == GRASS.calc_mu(t)
+    @test GRASS.make_grid(256) == GRASS.make_grid(N=256)
+    @test GRASS.calc_mu(t) == GRASS.calc_mu(t[1], t[2])
+    @test GRASS.calc_mu((0.0, 0.0)) == 1.0
+    @test GRASS.calc_mu((1.0, 2.5)) == 0.0
 end
 
 @testset "Testing off-disk coordinate" begin
     t = (1.25, 1.25)
-    @test iszero(SS.calc_mu(t))
-    @test SS.calc_r2(t) > one(eltype(t))
+    @test iszero(GRASS.calc_mu(t))
+    @test GRASS.calc_r2(t) > one(eltype(t))
 end
 
 @testset "Testing normalization" begin
     N = 50
     u1 = 0.0
     u2 = 0.0
-    @test SS.norm_term(0.0, 0.0, N, u1, u2) == pi ./ (2.0 * N^2)
+    @test GRASS.norm_term(0.0, 0.0, N, u1, u2) == pi ./ (2.0 * N^2)
 end
 
 end
