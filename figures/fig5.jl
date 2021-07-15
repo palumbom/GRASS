5d331401fd608c3c6e7ae38d0f8a137cd498e6d0
Michael Palumbo <palumbomi95@gmail.com>
2021-07-15 11:20:27 -0400

    ready to run on cluster

--- figures/fig5.jl
+++ figures/fig5.jl
@@ -19,9 +19,9 @@ mpl.style.use(GRASS.moddir * "figures/fig.mplstyle")
 include(GRASS.moddir * "figures/rms_loop.jl")
 
 # some global stuff
-const N = 48#N = 256
-const Nt = 48#Nt = 200
-const Nloop = 50#Nloop = 100
+const N = 256
+const Nt = 200
+const Nloop = 100
 
 function depth(;write=false)
     # set up parameters for lines
@@ -75,7 +75,6 @@ df = depth(write=true)
 arrowprops = Dict("facecolor"=>"black", "shrink"=>0.05, "width"=>2.0,"headwidth"=>8.0)
 fig, ax1 = plt.subplots()
 ax1.errorbar(df.depths, df.rms, yerr=df.std, capsize=3.0, color="black", fmt=".")
-# ax1.plot(df.depths, df.rms, color="black", marker=".")
 ax1.fill_betweenx(range(0.0, 1.0, length=5), zeros(5), zeros(5) .+ 0.2, color="k", alpha=0.25)
 ax1.set_xlabel(L"{\rm Line\ Depth}")
 ax1.set_ylabel(L"{\rm RMS\ RV\ (m s}^{-1})")
@@ -88,12 +87,8 @@ plt.clf(); plt.close()
 
 fig, ax1 = plt.subplots()
 ax1.errorbar(df.depths, df.avg_avg, yerr=df.avg_std, capsize=3.0, color="black", fmt=".")
-# ax1.fill_betweenx(range(0.0, 1.0, length=5), zeros(5), zeros(5) .+ 0.2, color="k", alpha=0.25)
 ax1.set_xlabel(L"{\rm Line\ Depth}")
 ax1.set_ylabel(L"{\rm RV\ (m s}^{-1})")
 ax1.set_xlim(0.0, 1.0)
-# ax1.set_ylim(0.19, 0.36)
-# ax1.annotate("Shallow", xy=(0.85,0.202), xytext=(0.05,0.2), arrowprops=arrowprops)
-# ax1.annotate("Deep", xy=(0.86, 0.2))
 fig.savefig("avg_vs_depth.pdf")
 plt.clf(); plt.close()
