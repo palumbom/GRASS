8607ccba02125f8e85de6aaecf368fb96528c4c0
Michael Palumbo <palumbomi95@gmail.com>
2021-07-15 14:52:24 -0400

    compare power spec

--- figures/fig8.jl
+++ figures/fig8.jl
@@ -2,8 +2,7 @@ using Pkg; Pkg.activate(".")
 using Statistics
 using GRASS
 using EchelleCCFs
-# using DSP
-# using FFTW
+using FFTW
 
 # plotting
 using LaTeXStrings
@@ -52,50 +51,48 @@ function power_spec_for_input(; mu::Symbol=:mu10, ax::Symbol=:c)
     v_grid, ccfs = calc_ccf(lambdas, flux, [5434.5232], [1-minimum(flux)], 7e5, normalize=true)
     rvs, sigs = calc_rvs_from_ccf(v_grid, ccfs)
 
-    return
+    # do a fourier transform
+    fourier = FFTW.fft(rvs) |> FFTW.fftshift
+    freqs = FFTW.fftfreq(length(rvs), 1.0/15.0) |> FFTW.fftshift
+    return freqs, abs.(fourier)
 end
 
+# now get the power spec for each disk position
 mu = :mu10
 ax = :c
-power_spec_for_input(mu=mu, ax=ax)
+freqs, power = power_spec_for_input(mu=mu, ax=ax)
+plt.loglog(freqs, power, label="Input data")
 
+# set up stuff for lines
+N = 256
+Nt = 500
+lines = [5434.5]
+depths = [0.8]
+variability = [true]
+resolution = 700000.0
+disk = DiskParams(N=N, Nt=Nt)
+spec = SpecParams(lines=lines, depths=depths, variability=variability,
+                  resolution=resolution, fixed_width=false,
+                  fixed_bisector=false, extrapolate=true,
+                  contiguous_only=false)
 
-# # set up stuff for lines
-# N = 256
-# Nt = 1000
-# lines = [5434.5]
-# depths = [0.8]
-# variability = [true]
-# resolution = 700000.0
-# disk = DiskParams(N=N, Nt=Nt)
-# spec = SpecParams(lines=lines, depths=depths, variability=variability,
-#                   resolution=resolution, fixed_width=false,
-#                   fixed_bisector=false, extrapolate=true,
-#                   contiguous_only=false)
+# synthesize spectra
+println(">>> Synthesizing spectra...")
+lambdas1, outspec1 = synthesize_spectra(spec, disk, seed_rng=false, verbose=true, top=NaN)
 
-# # synthesize spectra
-# println(">>> Synthesizing spectra...")
-# lambdas1, outspec1 = synthesize_spectra(spec, disk, seed_rng=false, verbose=true, top=NaN)
+# calculate the ccf
+println(">>> Calculating velocities...")
+v_grid, ccf1 = calc_ccf(lambdas1, outspec1, spec, normalize=true)
+rvs, sigs = calc_rvs_from_ccf(v_grid, ccf1)
 
-# # calculate the ccf
-# println(">>> Calculating velocities...")
-# v_grid, ccf1 = calc_ccf(lambdas1, outspec1, spec, normalize=true)
-# rvs, sigs = calc_rvs_from_ccf(v_grid, ccf1)
+# get frequencies to sample and then power
+println(">>> Getting periodogram...")
+fourier = FFTW.fft(rvs) |> FFTW.fftshift
+freqs = FFTW.fftfreq(length(rvs), 1.0/15.0) |> FFTW.fftshift
 
-# # import from astropy
-# u = pyimport("astropy.units")
-# ts = pyimport("astropy.timeseries")
-# xs = collect(range(15.0, 15.0 * Nt, length=Nt))
-# ys = rvs
-
-# # get frequencies to sample and then power
-# println(">>> Getting periodogram...")
-# freq = range(1e-6, 1e-1, length=32000)
-# power = ts.LombScargle(xs, ys).power(freq)
-
-# # plot it
-# plt.loglog(freq, power)
-# plt.title("Sampling = 15s, total time = " * string(Nt * 15) * " s")
-# plt.xlabel("Frequency (Hz)")
-# plt.ylabel("Power (units = ?)")
-# plt.show()
+# plot it
+plt.loglog(freqs, abs.(fourier), label="Synthetic")
+plt.title("Sampling = 15s, total time = " * string(Nt * 15) * " s")
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Power (units = ?)")
+plt.show()
