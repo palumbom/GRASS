215cd1db03e0685f60024f8ae89a552106c68dc3
Michael Palumbo <palumbomi95@gmail.com>
2021-07-15 11:00:47 -0400

    testing

--- figures/rms_loop.jl
+++ figures/rms_loop.jl
@@ -11,3 +11,17 @@
     end
     return mean(rms0), std(rms0)
 end
+
+# parallelized for loop
+@everywhere function avg_loop(spec::SpecParams, disk::DiskParams, Nloop::T; top::Float64=NaN) where T<:Integer
+    avg0 = zeros(Nloop)
+    for j in 1:Nloop
+        lambdas, outspec = synthesize_spectra(spec, disk, seed_rng=false, top=top)
+
+        # extract velocities
+        v_grid, ccf = calc_ccf(lambdas, outspec, spec, normalize=true)
+        rvs, sigs = calc_rvs_from_ccf(v_grid, ccf)
+        avg0[j] = mean(rvs)
+    end
+    return mean(avg0), std(avg0)
+end
