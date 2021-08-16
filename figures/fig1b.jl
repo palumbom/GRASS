93dc39aac9a1e2f77fda863fcebdb625ff6fe277
Michael Palumbo <palumbomi95@gmail.com>
2021-08-16 12:30:54 -0400

    write plotdir

--- figures/fig1b.jl
+++ figures/fig1b.jl
@@ -63,6 +63,7 @@ function plot_input_variability()
     if write
         fig.savefig(plotdir * "fig1b.pdf")
         plt.clf(); plt.close()
+        println(">>> Figure written to: " * plotdir * "fig1b.pdf")
     else
         plt.show()
         plt.clf(); plt.close()
