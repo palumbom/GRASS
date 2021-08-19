afd6f73882a9a7ef3cc95314e979dff240a40121
Michael Palumbo <palumbomi95@gmail.com>
2021-08-19 10:38:08 -0400

    consolidating functions

--- figures/fig_functions.jl
+++ figures/fig_functions.jl
@@ -1,31 +1,22 @@
 using Distributed
 
 # parallelized for loop
-@everywhere function rms_loop(spec::SpecParams, disk::DiskParams, Nloop::T; top::Float64=NaN) where T<:Integer
+@everywhere function spec_loop(spec::SpecParams, disk::DiskParams, Nloop::T; top::Float64=NaN) where T<:Integer
     rms0 = zeros(Nloop)
-    for j in 1:Nloop
-        lambdas, outspec = synthesize_spectra(spec, disk, seed_rng=false, top=top)
-
-        # extract velocities
-        v_grid, ccf = calc_ccf(lambdas, outspec, spec, normalize=true)
-        rvs, sigs = calc_rvs_from_ccf(v_grid, ccf)
-        rms0[j] = calc_rms(rvs)
-    end
-    return mean(rms0), std(rms0)
-end
-
-# parallelized for loop
-@everywhere function avg_loop(spec::SpecParams, disk::DiskParams, Nloop::T; top::Float64=NaN) where T<:Integer
     avg0 = zeros(Nloop)
     for j in 1:Nloop
+        # synthesize spectra
         lambdas, outspec = synthesize_spectra(spec, disk, seed_rng=false, top=top)
 
         # extract velocities
         v_grid, ccf = calc_ccf(lambdas, outspec, spec, normalize=true)
         rvs, sigs = calc_rvs_from_ccf(v_grid, ccf)
+
+        # compute rms and mean
+        rms0[j] = calc_rms(rvs)
         avg0[j] = mean(rvs)
     end
-    return mean(avg0), std(avg0)
+    return mean(avg0), std(avg0), mean(rms0), std(rms0)
 end
 
 # small function to check and create file structure
