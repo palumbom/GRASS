2f9093578f02dc6b2e554cd98522dd93937f78cf
Michael Palumbo <palumbomi95@gmail.com>
2021-09-02 15:57:45 -0400

    fixed fig 8 parallel issue

--- figures/fig7.jl
+++ figures/fig7.jl
@@ -156,7 +156,7 @@ if plot
     for i in eachindex(axs)
         # annotate axes and set labels, etc.
         axs[i].annotate(L"N_{\rm obs} =\ " * latexstring(N_obs[i]), xy=xy, bbox=bbox)
-        axs[i].set_xlabel(L"{\rm Measured\ velocity\ (m s}^{-1})")
+        axs[i].set_xlabel(L"{\rm Mean\ velocity\ (m s}^{-1})")
         axs[i].set_ylabel(L"{\rm Probability\ density}")
         axs[i].set_xlim(round(minimum(first.(xlims)), digits=0) - 0.25,
                         round(maximum(last.(xlims)), digits=0) + 0.25)
@@ -167,6 +167,53 @@ if plot
     # set legend in one panel and write figure
     axs[1].legend(loc="upper left")
     fig.savefig(plotdir * "fig7.pdf")
+    plt.clf(); plt.close()
+    println(">>> Figure written to: " * plotdir * "fig7.pdf")
+
+    # initialize plotting objects
+    fig = plt.figure(figsize=(10.5, 6.75))
+    ax1 = plt.subplot2grid((2,4),(0,0), colspan=2)
+    ax2 = plt.subplot2grid((2,4),(0,2), colspan=2)
+    ax3 = plt.subplot2grid((2,4),(1,1), colspan=2)
+    axs = [ax1, ax2, ax3]
+
+    # make arrays for limits and loop over plotting windows
+    xlims = []
+    ylims = []
+    for i in eachindex(axs)
+        # histogram for short dead time
+        axs[i].hist(rms_shortdead[i,:], density=true, histtype="stepfilled", fc="k", ec="k", lw=2, alpha=0.5, label="Short wait")
+        axs[i].hist(rms_shortdead[i,:], density=true, histtype="step", ec="k", lw=1.0, alpha=0.8)
+
+        # histogram for long wait time
+        axs[i].hist(rms_largedead[i,:], density=true, histtype="stepfilled", fc="tab:blue", ec="tab:blue", lw=1.5, alpha=0.5, label="Long wait")
+        axs[i].hist(rms_largedead[i,:], density=true, histtype="step", ec="tab:blue", lw=1.5, alpha=0.8)
+
+        # get axis limits
+        append!(xlims, [axs[i].get_xlim()])
+        append!(ylims, [axs[i].get_ylim()])
+
+        # report two-sample KS test
+        @show(ApproximateTwoSampleKSTest(rms_shortdead[i,:], rms_largedead[i,:]))
+    end
+
+    # set the axes limits
+    bbox = Dict("ec"=>"black", "fc"=>"white")
+    xy = (maximum(last.(xlims)) - 0.4, maximum(last.(ylims)) - 0.35)
+    for i in eachindex(axs)
+        # annotate axes and set labels, etc.
+        axs[i].annotate(L"N_{\rm obs} =\ " * latexstring(N_obs[i]), xy=xy, bbox=bbox)
+        axs[i].set_xlabel(L"{\rm RMS\ RV\ (m s}^{-1})")
+        axs[i].set_ylabel(L"{\rm Probability\ density}")
+        # axs[i].set_xlim(round(minimum(first.(xlims)), digits=0) - 0.25,
+        #                 round(maximum(last.(xlims)), digits=0) + 0.25)
+        # axs[i].set_ylim(minimum(first.(ylims)), maximum(last.(ylims)))
+        # axs[i].xaxis.set_major_locator(mpl.ticker.MaxNLocator(5))
+    end
+
+    # set legend in one panel and write figure
+    axs[1].legend(loc="upper left")
+    fig.savefig(plotdir * "fig7b.pdf")
     plt.clf(); plt.close()
     println(">>> Figure written to: " * plotdir * "fig7.pdf")
 end
