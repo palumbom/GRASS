e7e857908784a2ade6c71dcaf03221c139bcf5b0
Michael Palumbo <palumbomi95@gmail.com>
2021-09-15 12:24:53 -0400

    removed old todos's

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,5 @@ include("test_geometry.jl")
 include("test_physics.jl")
 include("test_ccf.jl")
 include("test_interpolations.jl")
-include("test_input.jl")  # TODO figure out this issue with automated testing
+include("test_input.jl")
 include("test_synthesis.jl")
