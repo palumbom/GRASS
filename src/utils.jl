842b1934e6c635af2805721c0d12084f24fe91ef
Michael Palumbo <palumbomi95@gmail.com>
2021-10-05 12:24:08 -0400

    add method for rms calculation along specified dimension

--- src/utils.jl
+++ src/utils.jl
@@ -32,17 +32,13 @@ Calculate the RMS of given data about the observed mean.
 # Arguments
 - `A::AbstractArray{Float64,1}`: 1D array containing data for RMS calculation
 """
-function calc_rms(A::AA{T,1}) where T<:AF
+function calc_rms(A::AA{T,1}) where T
     return sqrt(sum((A.-mean(A)).^2)/length(A))
 end
 
-# function calc_rms(A::AA{T,N}; dims::Integer) where {T,N}
-#     1 <= dims <= N || throw(ArgumentError("dimension $dims out of range (1:$N)"))
-#     r = axes(A)
-#     r0 = ntuple(i -> i == dims ? UnitRange(1, last(r[i]) - 1) : UnitRange(r[i]), N)
-#     r1 = ntuple(i -> i == dims ? UnitRange(2, last(r[i])) : UnitRange(r[i]), N)
-#     return sqrt(sum((A.-mean(A)).^2)/length(A))
-# end
+function calc_rms(A::AA{T,N}; dims::Integer) where {T,N}
+    return mapslices(calc_rms, A, dims=dims)
+end
 
 function strip_nans_by_column(A::AA{T,1}) where T<:AF
     nans = isnan.(A)
