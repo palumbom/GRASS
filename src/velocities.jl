6b0d96949c321a3abe87b3393efe48a2c213843b
Michael Palumbo <palumbomi95@gmail.com>
2021-10-06 11:36:14 -0400

    unzip function and mapslices

--- src/velocities.jl
+++ src/velocities.jl
@@ -46,7 +46,7 @@ end
 function calc_ccf(lambdas::AA{T,1}, intensities::AA{T,2}, lines::AA{T,1},
                   depths::AA{T,1}, resolution::T; normalize::Bool=true) where T<:AF
     func = x -> calc_ccf(lambdas, x, lines, depths, resolution, normalize=normalize)
-    out = map(func, intensities[:,i] for i in 1:size(intensities, 2))
+    out = mapslices(func, intensities, dims=1)
     return out[1][1], cat([x[2] for x in out]..., dims=2)
 end
 
@@ -56,8 +56,8 @@ end
 
 function calc_ccf(lambdas::AA{T,1}, intensities::AA{T,2}, spec::SpecParams{T}; normalize::Bool=true) where T<:AF
     func = x -> calc_ccf(lambdas, x, spec, normalize=normalize)
-    out = map(func, intensities[:,i] for i in 1:size(intensities, 2))
-    return out[1][1], cat([x[2] for x in out]..., dims=2)
+    outs = mapslices(func, intensities, dims=1)
+    return outs[1][1], cat([x[2] for x in outs]..., dims=2)
 end
 
 
@@ -77,6 +77,6 @@ end
 
 function calc_rvs_from_ccf(v_grid::AA{T,1}, ccf::AA{T,2}; frac_of_width_to_fit::T=0.65) where T<:AF
     func = x -> calc_rvs_from_ccf(v_grid, x, frac_of_width_to_fit=frac_of_width_to_fit)
-    out = map(func, ccf[:,i] for i in 1:size(ccf, 2))
-    return cat([x[1] for x in out]..., dims=1), cat([x[2] for x in out]..., dims=1)
+    out = mapslices(func, ccf, dims=1)
+    return vec.(unzip(out))#(getfield.(out, x) for x in fieldnames(eltype(out)))
 end
