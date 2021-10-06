1ea8a142b6ec919bfc41308bea715b6531c95e4b
Michael Palumbo <palumbomi95@gmail.com>
2021-10-06 15:07:10 -0400

    specify mask and fit shape for velocities

--- src/velocities.jl
+++ src/velocities.jl
@@ -13,15 +13,17 @@ Compute the cross correlation function from a spectrum (lambdas and intensities)
 - `depths::AbstractArray{Float64,1}`: List of line depths for use as weights in CCF mask.
 - `resolution::Float64`: Spectral resolution of spcectrum.
 """
-function calc_ccf(lambdas::AA{T,1}, intensities::AA{T,1}, lines::AA{T,1},
-                  depths::AA{T,1}, resolution::T; normalize::Bool=true) where T<:AF
+function calc_ccf(lambdas::AA{Float64,1}, intensities::AA{Float64,1},
+                  lines::AA{Float64,1}, depths::AA{Float64,1},
+                  resolution::Float64; normalize::Bool=true,
+                  mask_type::Type{T}=EchelleCCFs.TopHatCCFMask) where {T<:EchelleCCFs.AbstractCCFMaskShape}
     # make a line list
     line_list = EchelleCCFs.BasicLineList(lines, depths)
 
     # set mask properties
-    speed_of_light = c_ms::T
+    speed_of_light = c_ms
     mask_width = speed_of_light/resolution
-    mask_shape = EchelleCCFs.TopHatCCFMask(mask_width)
+    mask_shape = T(mask_width)
     midpoint = 0.0  # just by eye so things aren't wildly shifted
     max_bc = 0.0    #  ~ 2pi AU/year  in m/s
     Δv_step = 400   # m/s arbitrary probably smaller/slower than you need
@@ -43,19 +45,25 @@ function calc_ccf(lambdas::AA{T,1}, intensities::AA{T,1}, lines::AA{T,1},
     return v_grid, ccf
 end
 
-function calc_ccf(lambdas::AA{T,1}, intensities::AA{T,2}, lines::AA{T,1},
-                  depths::AA{T,1}, resolution::T; normalize::Bool=true) where T<:AF
-    func = x -> calc_ccf(lambdas, x, lines, depths, resolution, normalize=normalize)
+function calc_ccf(lambdas::AA{Float64,1}, intensities::AA{Float64,2},
+                  lines::AA{Float64,1}, depths::AA{Float64,1},
+                  resolution::Float64; normalize::Bool=true,
+                  mask_type::Type{T}=EchelleCCFs.TopHatCCFMask) where {T<:EchelleCCFs.AbstractCCFMaskShape}
+    func = x -> calc_ccf(lambdas, x, lines, depths, resolution, normalize=normalize, mask_type=mask_type)
     out = mapslices(func, intensities, dims=1)
     return out[1][1], cat([x[2] for x in out]..., dims=2)
 end
 
-function calc_ccf(lambdas::AA{T,1}, intensities::AA{T,1}, spec::SpecParams{T}; normalize::Bool=true) where T<:AF
-    return calc_ccf(lambdas, intensities, spec.lines, spec.depths, spec.resolution, normalize=normalize)
+function calc_ccf(lambdas::AA{Float64,1}, intensities::AA{Float64,1},
+                  spec::SpecParams{Float64}; normalize::Bool=true,
+                  mask_type::Type{T}=EchelleCCFs.TopHatCCFMask) where {T<:EchelleCCFs.AbstractCCFMaskShape}
+    return calc_ccf(lambdas, intensities, spec.lines, spec.depths, spec.resolution, normalize=normalize, mask_type=mask_type)
 end
 
-function calc_ccf(lambdas::AA{T,1}, intensities::AA{T,2}, spec::SpecParams{T}; normalize::Bool=true) where T<:AF
-    func = x -> calc_ccf(lambdas, x, spec, normalize=normalize)
+function calc_ccf(lambdas::AA{Float64,1}, intensities::AA{Float64,2},
+                  spec::SpecParams{Float64}; normalize::Bool=true,
+                  mask_type::Type{T}=EchelleCCFs.TopHatCCFMask) where {T<:EchelleCCFs.AbstractCCFMaskShape}
+    func = x -> calc_ccf(lambdas, x, spec, normalize=normalize, mask_type=mask_type)
     outs = mapslices(func, intensities, dims=1)
     return outs[1][1], cat([x[2] for x in outs]..., dims=2)
 end
@@ -70,13 +78,17 @@ Calculate apparent radial velocity from a CCF and velocity grid.
 - `v_grid::AbstractArray{Float64,1}`: List of velocities returned by calc_ccf.
 - `ccf::AbstractArray{Float64,1}`: CCF values returned by calc_ccf.
 """
-function calc_rvs_from_ccf(v_grid::AA{T,1}, ccf::AA{T,1}; frac_of_width_to_fit::T=0.65) where T<:AF
-    mrv = EchelleCCFs.MeasureRvFromCCFGaussian(frac_of_width_to_fit=frac_of_width_to_fit)
+function calc_rvs_from_ccf(v_grid::AA{Float64,1}, ccf::AA{Float64,1};
+                           frac_of_width_to_fit::Float64=0.65,
+                           fit_type::Type{T}=EchelleCCFs.MeasureRvFromCCFGaussian) where {T<:EchelleCCFs.AbstractMeasureRvFromCCF}
+    mrv = T(frac_of_width_to_fit=frac_of_width_to_fit)
     return mrv(v_grid, ccf)
 end
 
-function calc_rvs_from_ccf(v_grid::AA{T,1}, ccf::AA{T,2}; frac_of_width_to_fit::T=0.65) where T<:AF
-    func = x -> calc_rvs_from_ccf(v_grid, x, frac_of_width_to_fit=frac_of_width_to_fit)
+function calc_rvs_from_ccf(v_grid::AA{Float64,1}, ccf::AA{Float64,2};
+                           frac_of_width_to_fit::Float64=0.65,
+                           fit_type::Type{T}=EchelleCCFs.MeasureRvFromCCFGaussian) where {T<:EchelleCCFs.AbstractMeasureRvFromCCF}
+    func = x -> calc_rvs_from_ccf(v_grid, x, frac_of_width_to_fit=frac_of_width_to_fit, fit_type=fit_type)
     out = mapslices(func, ccf, dims=1)
     return vec.(unzip(out))
 end
