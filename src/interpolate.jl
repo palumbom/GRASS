e694eada8e3ad4c13463ae16ede00a2ce7c0d916
Michael Palumbo <palumbomi95@gmail.com>
2021-10-11 14:44:14 -0400

    my own interpolation

---
+++ src/interpolate.jl
@@ -0,0 +1,13 @@
+function linear_interp(xs::AbstractArray{T,1}, ys::AbstractArray{T,1}; bc::T=1.0) where T<:Float64
+    function f(x)
+        if ((x <= first(xs)) | (x >= last(xs)))
+            return bc
+        else
+            i = searchsortedfirst(xs, x) - 1
+            i0 = clamp(i, firstindex(ys), lastindex(ys))
+            i1 = clamp(i+1, firstindex(ys), lastindex(ys))
+            return ys[i0] + (ys[i1] - ys[i0]) * (x - xs[i0]) / (xs[i1] - xs[i0])
+        end
+    end
+    return f
+end
