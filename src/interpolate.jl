13ce49043ecb23a3a3b230b3508218b5d951a5ad
Michael Palumbo <palumbomi95@gmail.com>
2021-10-12 12:14:40 -0400

    new interpolation for trim

--- src/interpolate.jl
+++ src/interpolate.jl
@@ -1,7 +1,11 @@
 function linear_interp(xs::AbstractArray{T,1}, ys::AbstractArray{T,1}; bc::T=1.0) where T<:Float64
     function f(x)
-        if ((x <= first(xs)) | (x >= last(xs)))
-            return bc
+        # if ((x <= first(xs)) | (x >= last(xs)))
+        #     return bc
+        if x <= first(xs)
+            return first(ys)
+        elseif x >= last(xs)
+            return last(ys)
         else
             i = searchsortedfirst(xs, x) - 1
             i0 = clamp(i, firstindex(ys), lastindex(ys))
