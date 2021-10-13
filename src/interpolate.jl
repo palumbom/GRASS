45405989eabb001ff124aeaf6b15c9ba4364217c
Michael Palumbo <palumbomi95@gmail.com>
2021-10-13 15:33:37 -0400

    fixed interpolate bug + gpu

--- src/interpolate.jl
+++ src/interpolate.jl
@@ -1,6 +1,8 @@
+# TODO: @code_warntype
+# TODO: jet test package?
 function linear_interp(xs::AbstractArray{T,1}, ys::AbstractArray{T,1}; bc::T=NaN) where T<:Float64
     function f(x)
-        if (((x <= first(xs)) | (x >= last(xs))) & !isnan(bc))
+        if (((x < first(xs)) | (x > last(xs))) & !isnan(bc))
             return bc
         elseif x <= first(xs)
             return first(ys)
@@ -11,11 +13,8 @@ function linear_interp(xs::AbstractArray{T,1}, ys::AbstractArray{T,1}; bc::T=NaN
             i = searchsortedfirst(xs, x) - 1
             i0 = clamp(i, firstindex(ys), lastindex(ys))
             i1 = clamp(i+1, firstindex(ys), lastindex(ys))
-            return ys[i0] + (ys[i1] - ys[i0]) * ((x - xs[i0]) / (xs[i1] - xs[i0]))
+            return ys[i0] + (ys[i1] - ys[i0]) * (x - xs[i0]) / (xs[i1] - xs[i0])
         end
     end
     return f
 end
-
-# TODO: @code_warntype
-# TODO: jet test package?
