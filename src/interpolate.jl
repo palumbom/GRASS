c6fda08475dfd7bdc9eb0bd1cba635e1811abf73
Michael Palumbo <palumbomi95@gmail.com>
2021-10-13 11:40:27 -0400

    interp

--- src/interpolate.jl
+++ src/interpolate.jl
@@ -7,11 +7,15 @@ function linear_interp(xs::AbstractArray{T,1}, ys::AbstractArray{T,1}; bc::T=NaN
         elseif x >= last(xs)
             return last(ys)
         else
+            # todo: pass "hint" to searchsorted first
             i = searchsortedfirst(xs, x) - 1
             i0 = clamp(i, firstindex(ys), lastindex(ys))
             i1 = clamp(i+1, firstindex(ys), lastindex(ys))
-            return ys[i0] + (ys[i1] - ys[i0]) * (x - xs[i0]) / (xs[i1] - xs[i0])
+            return ys[i0] + (ys[i1] - ys[i0]) * ((x - xs[i0]) / (xs[i1] - xs[i0]))
         end
     end
     return f
 end
+
+# TODO: @code_warntype
+# TODO: jet test package?
