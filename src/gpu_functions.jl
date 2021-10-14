adf415418d021a1bfff6460d8d1fd83f8df87b5e
Michael Palumbo <palumbomi95@gmail.com>
2021-10-14 14:53:06 -0400

    working on parallelizing gpu functions

--- src/gpu_functions.jl
+++ src/gpu_functions.jl
@@ -1,6 +1,6 @@
 function linear_interp_gpu(out, new_xs, xs, ys, bc)
-    # get GPU dims
-    i = threadIdx().x + blockDim().x * (blockIdx().x-1)
+    # # get GPU dims
+    # i = threadIdx().x + blockDim().x * (blockIdx().x-1)
 
     # perform the interpolation
     n = CUDA.length(new_xs)
@@ -16,6 +16,29 @@ function linear_interp_gpu(out, new_xs, xs, ys, bc)
             j0 = CUDA.clamp(j, CUDA.firstindex(ys), CUDA.lastindex(ys))
             j1 = CUDA.clamp(j+1, CUDA.firstindex(ys), CUDA.lastindex(ys))
             out[i] = ys[j0] + (ys[j1] - ys[j0]) * (new_xs[i] - xs[j0]) / (xs[j1] - xs[j0])
+        end
+    end
+    return nothing
+end
+
+function linear_interp_mult_gpu(out, new_xs, xs, ys, bc)
+    # # get GPU dims
+    # i = threadIdx().x + blockDim().x * (blockIdx().x-1)
+
+    # perform the interpolation
+    n = CUDA.length(new_xs)
+    for i in 1:CUDA.length(new_xs)
+        if (((new_xs[i] < CUDA.first(xs)) | (new_xs[i] > CUDA.last(xs))) & !CUDA.isnan(bc))
+            out[i] *= bc
+        elseif new_xs[i] <= CUDA.first(xs)
+            out[i] *= CUDA.first(ys)
+        elseif new_xs[i] >= CUDA.last(xs)
+            out[i] *= CUDA.last(ys)
+        else
+            j = CUDA.searchsortedfirst(xs, new_xs[i]) - 1
+            j0 = CUDA.clamp(j, CUDA.firstindex(ys), CUDA.lastindex(ys))
+            j1 = CUDA.clamp(j+1, CUDA.firstindex(ys), CUDA.lastindex(ys))
+            out[i] *= ys[j0] + (ys[j1] - ys[j0]) * (new_xs[i] - xs[j0]) / (xs[j1] - xs[j0])
         end
     end
     return nothing
@@ -51,26 +74,57 @@ function trim_bisector_chop_gpu!(depth, wavt, bist, dept, widt, top)
     return nothing
 end
 
-function line_loop_gpu(prof, mid, depth, rot_shift, conv_blueshift, lambdas, wavt, bist, dept, widt, top)
-    # first trim the bisectors to the correct depth
-    trim_bisector_chop_gpu!(depth, wavt, bist, dept, widt, top)
+function line_loop_gpu(prof, lines, depths, rot_shift, conv_blueshift, lambdas, wavt, bist, dept, widt, lwavgrid, rwavgrid, allwavs, allints, top)
+    # get GPU dims
+    ix = threadIdx().x + blockDim().x * (blockIdx().x-1)
+    sx = blockDim().x * gridDim().x
 
-    # calculate line center given rot. and conv. doppler shift -> λrest * (1 + z)
-    λΔD = mid * (1.0 + rot_shift) * (1.0 + conv_blueshift)
+    # synthesize the line
+    for i in ix:sx:length(lines)
+        # slice out the data
+        wavt1 = CUDA.view(wavt, :, i)
+        bist1 = CUDA.view(bist, :, i)
+        dept1 = CUDA.view(dept, :, i)
+        widt1 = CUDA.view(widt, :, i)
 
-    # find window around shifted line
-    lind = CUDA.findfirst(x -> x > λΔD - 0.5, lambdas)
-    rind = CUDA.findfirst(x -> x > λΔD + 0.5, lambdas)
+        # slice out the correct spot in memory
+        lwavgrid1 = CUDA.view(lwavgrid, :, i)
+        rwavgrid1 = CUDA.view(rwavgrid, :, i)
+        allwavs1 = CUDA.view(allwavs, :, i)
+        allints1 = CUDA.view(allints, :, i)
 
+        # first trim the bisectors to the correct depth
+        trim_bisector_chop_gpu!(depths[i], wavt1, bist1, dept1, widt1, top)
 
+        # calculate line center given rot. and conv. doppler shift -> λrest * (1 + z)
+        λΔD = lines[i] * (1.0 + rot_shift) * (1.0 + conv_blueshift)
 
+        # update the line profile in place
+        line_profile_gpu!(λΔD, lambdas, prof, wavt1, dept1, widt1, lwavgrid1, rwavgrid1, allwavs1, allints1)
+    end
     return nothing
 end
 
-function line_profile_gpu!(mid, lambdas, prof, wavt, dept, widt, lwavgrid, rwavgrid, allwavs, allints)
+function line_profile_gpu!(mid, lambdas, prof, wavm, depm, widm, lwavgrid, rwavgrid, allwavs, allints)
+    # set wavgrids to line center to start
+    for i in 1:CUDA.length(lwavgrid)
+        lwavgrid[i] = (mid - (0.5 * widm[i] - wavm[i]))
+        rwavgrid[i] = (mid + (0.5 * widm[i] + wavm[i]))
+    end
+    rwavgrid[1] = lwavgrid[1] + 1e-3            # TODO: fix to deal with nodes
 
+    # concatenate into one big array
+    len = CUDA.length(rwavgrid)
+    for i in 1:CUDA.length(rwavgrid)
+        allwavs[i+len] = rwavgrid[i]
+        allints[i+len] = depm[i]
+        allwavs[i] = lwavgrid[CUDA.length(rwavgrid) - (i - 1)]
+        allints[i] = depm[CUDA.length(rwavgrid) - (i - 1)]
+    end
 
-    return
+    # interpolate onto original lambda grid, extrapolate to continuum
+    linear_interp_mult_gpu(prof, lambdas, allwavs, allints, 1.0)
+    return nothing
 end
 
 using Pkg; Pkg.activate(".")
@@ -80,24 +134,76 @@ using JLD2
 using FileIO
 using DataFrames
 
+# get data
 data = GRASS.SolarData()
 wavt_main = data.wav[(:c, :mu10)][:,1]
 bist_main = data.bis[(:c, :mu10)][:,1]
 dept_main = data.dep[(:c, :mu10)][:,1]
 widt_main = data.wid[(:c, :mu10)][:,1]
 
-wavt1 = CuArray(wavt_main)
-bist1 = CuArray(bist_main)
-dept1 = CuArray(dept_main)
-widt1 = CuArray(widt_main)
+# values
+top = NaN
+dep = 0.75
+rot_shift = 0.0
+conv_blueshift = 0.0
+mid = 5434.5
 
-const top = NaN
-const dep = 0.75
-const rot_shift = 0.0
-const conv_blueshift = 0.0
-const mid = 5434.5
-lambdas = CuArray(range(mid-0.75, mid+0.75, step=mid/7e5))
-prof = CuArray(ones(length(lambdas)))
+# CPU stuff
+wsp = GRASS.SynthWorkspace(ndepths=100);
+wavt_cpu = copy(wavt_main); wsp.wavt .= wavt_cpu
+bist_cpu = copy(bist_main); wsp.bist .= bist_cpu
+dept_cpu = copy(dept_main); wsp.dept .= dept_cpu
+widt_cpu = copy(widt_main); wsp.widt .= widt_cpu
 
-# @cuda trim_bisector_chop_gpu!(dep, wavt1, bist1, dept1, widt1, NaN)
-@cuda line_loop_gpu(prof, mid, dep, rot_shift, conv_blueshift, lambdas, wavt1, bist1, dept1, widt1, top)
+lambdas_cpu = range(mid-2.0, mid+2.0, step=mid/7e5);
+prof_cpu = ones(length(lambdas_cpu));
+allwavs_cpu = zeros(200);
+allints_cpu = zeros(200);
+lwavgrid_cpu = zeros(100);
+rvwavgrid_cpu = zeros(100);
+
+# GPU stuff
+lines = CuArray([5434.5, 5434.7])
+depths = CuArray([0.5, 0.75])
+
+wavt_gpu = CuArray(repeat(wavt_main, 1, length(lines)))
+bist_gpu = CuArray(repeat(bist_main, 1, length(lines)))
+dept_gpu = CuArray(repeat(dept_main, 1, length(lines)))
+widt_gpu = CuArray(repeat(widt_main, 1, length(lines)))
+
+lambdas_gpu = CuArray(range(mid-2.0, mid+2.0, step=mid/7e5));
+prof_gpu = CUDA.ones(Float64, length(lambdas_gpu));
+allwavs_gpu = CUDA.zeros(Float64, 200, CUDA.length(lines));
+allints_gpu = CUDA.zeros(Float64, 200, CUDA.length(lines));
+lwavgrid_gpu = CUDA.zeros(Float64, 100, CUDA.length(lines));
+rwavgrid_gpu = CUDA.zeros(Float64, 100, CUDA.length(lines));
+
+# do the CPU
+# GRASS.trim_bisector_chop!(dep, wavt_cpu, bist_cpu, dept_cpu, widt_cpu, top=top)
+GRASS.line_loop(prof_cpu, mid, dep, rot_shift, conv_blueshift, lambdas_cpu, wsp)
+
+# do the GPU
+# @cuda trim_bisector_chop_gpu!(dep, wavt_gpu, bist_gpu, dept_gpu, widt_gpu, top)
+@cuda line_loop_gpu(prof_gpu, lines, depths, rot_shift, conv_blueshift, lambdas_gpu, wavt_gpu, bist_gpu, dept_gpu, widt_gpu, lwavgrid_gpu, rwavgrid_gpu, allwavs_gpu, allints_gpu, top)
+
+# function bmark_cpu()
+
+#     return
+# end
+
+# function bmark_gpu()
+
+
+#     return
+# end
+
+# function compare(gpu, cpu)
+#     println(maximum(abs.(Array(gpu) .- cpu)))
+# end
+
+# compare(wavt_gpu, wsp.wavt)
+# compare(bist_gpu, wsp.bist)
+# compare(dept_gpu, wsp.dept)
+# compare(widt_gpu, wsp.widt)
+# compare(prof_gpu, prof_cpu)
+
