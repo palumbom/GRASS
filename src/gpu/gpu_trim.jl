d5856a167a55a6c2708cc4d7182e1e988a44cd1a
Michael Palumbo <palumbomi95@gmail.com>
2021-11-11 14:19:56 -0500

    using approach of more kernel launches

--- src/gpu/gpu_trim.jl
+++ src/gpu/gpu_trim.jl
@@ -1,29 +1,55 @@
-function trim_bisector_chop_gpu!(depth, wavt, bist, dept, widt, top)
-    # replace spurious measurements at top of bisector
-    ind1 = CUDA.searchsortedfirst(bist, 1.0 - depth)
+function trim_bisector_chop_gpu!(depth, wavall, bisall, depall, widall, top)
+    # get indices from GPU blocks + threads
+    idx = threadIdx().x + blockDim().x * (blockIdx().x-1)
+    sdx = blockDim().x * gridDim().x
+    idy = threadIdx().y + blockDim().y * (blockIdx().y-1)
+    sdy = blockDim().y * gridDim().y
 
-    # TODO this will kill the code
-    if !CUDA.isnan(top)
-        ind2 = CUDA.searchsortedfirst(bist, top)
-        wavt[ind2:end] .= wavt[ind2]
-    end
+    # loop over epochs of bisectors
+    for i in idx:sdx:CUDA.size(wavall,2)
+        # get views of the correct slice
+        wavt = CUDA.view(wavall, :, i)
+        bist = CUDA.view(bisall, :, i)
+        dept = CUDA.view(depall, :, i)
+        widt = CUDA.view(widall, :, i)
 
-    # get knots
-    xs = CUDA.view(bist, ind1:CUDA.length(bist))
-    ys = CUDA.view(wavt, ind1:CUDA.length(wavt))
+        # find first index above specified depth
+        ind1 = CUDA.searchsortedfirst(bist, 1.0 - depth)
 
-    # get new grid of depths
-    step = depth/(CUDA.length(dept) - 1)
-    for i in 1:CUDA.length(dept)
-        dept[i] = (1.0 - depth) + (i-1) * step
-    end
+        # TODO this will kill the code
+        if !CUDA.isnan(top)
+            ind2 = CUDA.searchsortedfirst(bist, top)
+            wavt[ind2:end] .= wavt[ind2]
+        end
 
-    # do the interpolation
-    linear_interp_gpu(wavt, dept, xs, ys, NaN)
+        # get knots
+        xs = CUDA.view(bist, ind1:CUDA.length(bist))
+        ys = CUDA.view(wavt, ind1:CUDA.length(wavt))
 
-    # now assign bisector fluxes from dept
-    for i in 1:CUDA.length(bist)
-        bist[i] = dept[i]
+        # loop over the length of the bisector
+        step = depth/(CUDA.length(dept) - 1)
+        for j in idy:sdy:CUDA.size(wavall,1)
+            # set the new depth value=
+            new_dept = (1.0 - depth) + (j-1) * step
+
+            # interpolate to get the new wavelength value
+            if (((new_dept < CUDA.first(xs)) | (new_dept > CUDA.last(xs))) & !CUDA.isnan(top))
+                wavt[j] = top
+            elseif new_dept <= CUDA.first(xs)
+                wavt[j] = CUDA.first(ys)
+            elseif new_dept >= CUDA.last(xs)
+                wavt[j] = CUDA.last(ys)
+            else
+                k = CUDA.searchsortedfirst(xs, new_dept) - 1
+                k0 = CUDA.clamp(k, CUDA.firstindex(ys), CUDA.lastindex(ys))
+                k1 = CUDA.clamp(k+1, CUDA.firstindex(ys), CUDA.lastindex(ys))
+                wavt[j] = ys[k0] + (ys[k1] - ys[k0]) * (new_dept - xs[k0]) / (xs[k1] - xs[k0])
+            end
+
+            # assign bisector fluxes from dept
+            bist[j] = new_dept
+            dept[j] = new_dept
+        end
     end
     return nothing
 end
