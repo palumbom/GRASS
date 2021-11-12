49f67a5dfe24b0125d7ab7fc6652a1e3236ccbce
Michael Palumbo <palumbomi95@gmail.com>
2021-11-12 08:53:20 -0500

    gonna start testing gpu

--- src/GRASS.jl
+++ src/GRASS.jl
@@ -60,6 +60,43 @@ include("gpu/gpu_data.jl")
 include("gpu/gpu_trim.jl")
 include("gpu/gpu_sim.jl")
 
+"""
+    synthesize_spectra(spec, disk; seed_rng=false, verbose=true, top=NaN)
+
+Synthesize spectra given parameters in `spec` and `disk` instances.
+
+# Arguments
+- `spec::SpecParams`: SpecParams instance
+- `disk::DiskParams`: DiskParams instance
+"""
+function synthesize_spectra(spec::SpecParams, disk::DiskParams;
+                            top::Float64=NaN, seed_rng::Bool=false,
+                            verbose::Bool=true, use_gpu::Bool=false)
+    # parse out dimensions for memory allocation
+    N = disk.N
+    Nλ = length(spec.lambdas)
+
+    # allocate memory for output
+    outspec = zeros(Nλ, disk.Nt)
+
+    # call appropriate simulation function on cpu or gpu
+    if use_gpu
+        # make sure there is actually a GPU
+        @assert CUDA.functional()
+
+        # run the simulation and return
+        disk_sim_gpu(spec, disk, outspec)
+        return spec.lambdas, outspec
+    else
+        # allocate memory for synthesis
+        prof = ones(Nλ)
+
+        # run the simulation (outspec modified in place)
+        disk_sim(spec, disk, prof, outspec, seed_rng=seed_rng, verbose=verbose, top=top)
+        return spec.lambdas, outspec
+    end
+end
+
 # export some stuff
 export SpecParams, DiskParams, synthesize_spectra, calc_ccf, calc_rvs_from_ccf, calc_rms, use_gpu
 
