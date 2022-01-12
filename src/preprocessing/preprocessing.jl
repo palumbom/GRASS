9bfabc5ec93edf57863decc62284e658ef64513d
Michael Palumbo <palumbomi95@gmail.com>
2022-01-12 10:19:47 -0500

    generalizing preprocessing

--- src/preprocessing/preprocessing.jl
+++ src/preprocessing/preprocessing.jl
@@ -1,13 +1,34 @@
 using LsqFit
 
-function calc_width_at_depth(wavs::AA{T,1}, spec::AA{T,1};
+function calc_width_at_depth(wavs::AA{T,1}, spec::AA{T,1}; center::T=NaN,
                              len::Integer=100) where T<:Real
     # check that the length of arrays match
+    @assert !isnan(center)
     @assert length(wavs) == length(spec)
 
+    # get better center estimate
+    if (center - 1.0) > minimum(wavs)
+        lwingλ_idx = firstindex(wavs)
+    else
+        lwingλ_idx = findfirst(x -> x >= center - 1.0, wavs)
+    end
+
+    if (center + 1.0) > maximum(wavs)
+        rwingλ_idx = lastindex(wavs)
+    else
+        rwingλ_idx = findfirst(x -> x >= center + 1.0, wavs)
+    end
+    center_idx = argmin(view(spec, lwingλ_idx:rwingλ_idx)) + lwingλ_idx
+    center = wavs[center_idx]
+
+    # get better wing estimate
+    buffer = 0.4
+    lwingλ = center - buffer
+    rwingλ = center + buffer
+
     # hard-coded indices to narrow region of interest
-    lwavind = searchsortednearest(wavs, 5434.15)
-    rwavind = searchsortednearest(wavs, 5434.9)
+    lwavind = searchsortednearest(wavs, lwingλ)
+    rwavind = searchsortednearest(wavs, rwingλ)
     newwavs = wavs[lwavind:rwavind]
     newspec = spec[lwavind:rwavind]
     newspec ./= maximum(newspec)
@@ -19,8 +40,8 @@ function calc_width_at_depth(wavs::AA{T,1}, spec::AA{T,1};
     ind2 = findfirst(x -> x .>= topint, newspec[botind:end]) + botind
 
     # get model for line wings
-    lwing_wavs, lwing_flux = fit_line_wings(newwavs, newspec, side="left")
-    rwing_wavs, rwing_flux = fit_line_wings(newwavs, newspec, side="right")
+    lwing_wavs, lwing_flux = fit_line_wings(newwavs, newspec, center=center, side="left")
+    rwing_wavs, rwing_flux = fit_line_wings(newwavs, newspec, center=center, side="right")
 
     # replace data wings with model wings
     newwavs[1:ind1] .= lwing_wavs[1:ind1]
@@ -36,13 +57,16 @@ function calc_width_at_depth(wavs::AA{T,1}, spec::AA{T,1};
     return dep, wid
 end
 
-function calc_width_at_depth(wavs::AA{T,2}, spec::AA{T,2};
+function calc_width_at_depth(wavs::AA{T,2}, spec::AA{T,2}; center::T=NaN,
                              len::Integer=100) where T<:Real
-    out = map(calc_width_at_depth, eachcol(wavs), eachcol(spec))
+    f = (x,y) -> calc_width_at_depth(x, y, len=len, center=center)
+    out = map(f, eachcol(wavs), eachcol(spec))
     return cat([x[1] for x in out]..., dims=2), cat([x[2] for x in out]..., dims=2)
 end
 
-function fit_line_wings(wavs, spec; side="left")
+function fit_line_wings(wavs, spec; center=NaN, side="left")
+    @assert !isnan(center)
+
     # cut out the middle
     botind = argmin(spec)
     if side == "left"
@@ -54,7 +78,7 @@ function fit_line_wings(wavs, spec; side="left")
     end
 
     # perform the fit
-    p0 = [-0.125, 5434.5232, 0.03, 0.03, 1.0]
+    p0 = [-0.125, center, 0.03, 0.03, 1.0]
     fit = curve_fit(fit_voigt, wavs_fit, spec_fit, p0)
     return wavs, fit_voigt(wavs, fit.param)
 end
