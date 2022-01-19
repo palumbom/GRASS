2af74cd06e392ef0796accab3cefef8c6454372d
Michael Palumbo <palumbomi95@gmail.com>
2022-01-19 10:15:49 -0500

    organizing data

--- src/structures/SolarData.jl
+++ src/structures/SolarData.jl
@@ -5,7 +5,100 @@ struct SolarData{T1<:AF} <: AbstractSolarData
     dep::Dict{Tuple{Symbol,Symbol}, AbstractArray{T1,2}}
     wid::Dict{Tuple{Symbol,Symbol}, AbstractArray{T1,2}}
     len::Dict{Tuple{Symbol,Symbol}, Int}
-    line::AbstractLineProperties
+end
+
+"""
+    SolarData(; dir=soldir, relative=true, ...)
+
+Construct a `SpecParams` composite type instance.
+
+# Arguments
+- `dir::String=soldir`: Directory containing the pre-processed input data. Default directory is set in src/config.jl
+- `relative::Bool=true`: Set whether wavelengths are on absolute scale or expressed relative to rest wavelength.
+"""
+function SolarData(;dir::String=soldir, relative::Bool=true,
+                   fixed_width::Bool=false, fixed_bisector::Bool=false,
+                   extrapolate::Bool=true, contiguous_only::Bool=false,
+                   adjust_mean::Bool=true)
+    # sort the data directories and such
+    idf = sort_input_data(dir=dir)
+
+    # fill in line properties
+    lp = []
+    for path in unique(idf.fpath)
+        prop_file = glob("*_line_properties.h5", path)
+        push!(lp, LineProperties(prop_file[1]))
+    end
+
+    # allocate memory for data dictionaries
+    wavdict = Dict{Tuple{Symbol,Symbol}, AA{Float64,2}}()
+    bisdict = Dict{Tuple{Symbol,Symbol}, AA{Float64,2}}()
+    depdict = Dict{Tuple{Symbol,Symbol}, AA{Float64,2}}()
+    widdict = Dict{Tuple{Symbol,Symbol}, AA{Float64,2}}()
+    lengths = Dict{Tuple{Symbol,Symbol}, Int}()
+
+    # loop over unique mu + axis pairs
+    for ax in unique(idf.axis)
+        for mu in unique(idf.mu)
+            # get data for mu + axis pair
+            idf_temp = idf[((idf.axis .== ax) .& (idf.mu .== mu)), :]
+
+            # move on if no data for mu + axis pair
+            if isempty(idf_temp)
+                continue
+            end
+
+            # stitch the time series
+            wavall, bisall, depall, widall = stitch_time_series(idf_temp, adjust_mean=adjust_mean, contiguous_only=contiguous_only)
+
+            # clean the input
+            wavall, bisall, depall, widall = clean_input(wavall, bisall, depall, widall)
+
+            # deal with NaNs in wavall measurements
+            if extrapolate
+                extrapolate_bisector(wavall, bisall)
+            end
+
+            # assign key-value pairs to dictionary
+            if relative
+                relwav = relative_bisector_wavelengths(wavall, lp[1])
+                wavdict[(Symbol(ax), Symbol(mu))] = relwav
+            else
+                wavdict[(Symbol(ax), Symbol(mu))] = wavall
+            end
+            bisdict[(Symbol(ax), Symbol(mu))] = bisall
+
+            # assign width
+            if fixed_width
+                dep, wid = calc_fixed_width()
+                depdict[(Symbol(ax), Symbol(mu))] = zeros(size(depall))
+                widdict[(Symbol(ax), Symbol(mu))] = zeros(size(depall))
+                depdict[(Symbol(ax), Symbol(mu))] .= dep
+                widdict[(Symbol(ax), Symbol(mu))] .= wid
+            else
+                depdict[(Symbol(ax), Symbol(mu))] = depall
+                widdict[(Symbol(ax), Symbol(mu))] = widall
+            end
+
+            # set bisector to zero if fixed
+            if fixed_bisector
+                wavdict[(Symbol(ax), Symbol(mu))] = zeros(size(wavall))
+            end
+
+            # get length
+            lengths[(Symbol(ax), Symbol(mu))] = size(wavall,2)
+        end
+    end
+    return SolarData(wavdict, bisdict, depdict, widdict, lengths, lp[1])
+end
+
+function calc_fixed_width(;λ::T1=5434.5232, M::T1=26.0, T::T1=5778.0, v_turb::T1=3.5e5) where T1<:AF
+    wid = width_thermal(λ=λ, M=M, T=T, v_turb=v_turb)
+    λs = collect(range(λ - 1.0, λ + 1.0, step=λ/7e5))
+    prof = gaussian_line.(λs, mid=λ, width=wid, depth=0.86)
+    depall, widall = calc_width_at_depth(λs, prof, len=100)
+    widall[1] = 0.01
+    return depall, widall
 end
 
 function clean_input(wavall::AA{T,2}, bisall::AA{T,2}, depall::AA{T,2}, widall::AA{T,2}) where T<:AF
@@ -65,21 +158,6 @@ function extrapolate_bisector(wavall::AA{T,2}, bisall::AA{T,2}; top::T=0.8) wher
     return nothing
 end
 
-function extrapolate_width(depall::AA{T,2}, widall::AA{T,2}) where T<:AF
-    for i in 1:size(widall,2)
-        # take slice for one time snapshot
-        widt = view(widall, :, i)
-        dept = view(depall, :, i)
-
-        # fix the last value if necessary
-        if (widt[end-1] == widt[end])
-            dydx = (widt[end-1]/widt[end-2])/(dept[end-1]/dept[end-2])
-            widall[end, i] = widt[end-2] / dydx + widt[end-2]
-        end
-    end
-    return nothing
-end
-
 function relative_bisector_wavelengths(wav::AA{T,2}, lp::LineProperties) where T<:AF
     # allocate array of size(wav)
     relwav = similar(wav)
@@ -88,99 +166,4 @@ function relative_bisector_wavelengths(wav::AA{T,2}, lp::LineProperties) where T
         relwav[:,i] = wav[:,i] .- lp.λrest .- λgrav
     end
     return relwav
-end
-
-"""
-    SolarData(; dir=soldir, relative=true, ...)
-
-Construct a `SpecParams` composite type instance.
-
-# Arguments
-- `dir::String=soldir`: Directory containing the pre-processed input data. Default directory is set in src/config.jl
-- `relative::Bool=true`: Set whether wavelengths are on absolute scale or expressed relative to rest wavelength.
-"""
-function SolarData(;dir::String=soldir, relative::Bool=true,
-                   fixed_width::Bool=false, fixed_bisector::Bool=false,
-                   extrapolate::Bool=true, contiguous_only::Bool=false,
-                   adjust_mean::Bool=true)
-    # sort the data directories and such
-    idf = sort_input_data(dir=dir)
-
-    # fill in line properties
-    lp = []
-    for path in unique(idf.fpath)
-        prop_file = glob("*_line_properties.h5", path)
-         push!(lp, LineProperties(prop_file[1]))
-    end
-
-    # allocate memory for data dictionaries
-    wavdict = Dict{Tuple{Symbol,Symbol}, AA{Float64,2}}()
-    bisdict = Dict{Tuple{Symbol,Symbol}, AA{Float64,2}}()
-    depdict = Dict{Tuple{Symbol,Symbol}, AA{Float64,2}}()
-    widdict = Dict{Tuple{Symbol,Symbol}, AA{Float64,2}}()
-    lengths = Dict{Tuple{Symbol,Symbol}, Int}()
-
-    # loop over unique mu + axis pairs
-    for ax in unique(idf.axis)
-        for mu in unique(idf.mu)
-            # get data for mu + axis pair
-            idf_temp = idf[((idf.axis .== ax) .& (idf.mu .== mu)), :]
-
-            # move on if no data for mu + axis pair
-            if isempty(idf_temp)
-                continue
-            end
-
-            # stitch the time series
-            wavall, bisall, depall, widall = stitch_time_series(idf_temp, adjust_mean=adjust_mean, contiguous_only=contiguous_only)
-
-            # clean the input
-            wavall, bisall, depall, widall = clean_input(wavall, bisall, depall, widall)
-
-            # deal with NaNs in wavall measurements
-            if extrapolate
-                extrapolate_bisector(wavall, bisall)
-                # extrapolate_width(depall, widall)
-            end
-
-            # assign key-value pairs to dictionary
-            if relative
-                relwav = relative_bisector_wavelengths(wavall, lp[1])
-                wavdict[(Symbol(ax), Symbol(mu))] = relwav
-            else
-                wavdict[(Symbol(ax), Symbol(mu))] = wavall
-            end
-            bisdict[(Symbol(ax), Symbol(mu))] = bisall
-
-            # assign width
-            if fixed_width
-                dep, wid = calc_fixed_width()
-                depdict[(Symbol(ax), Symbol(mu))] = zeros(size(depall))
-                widdict[(Symbol(ax), Symbol(mu))] = zeros(size(depall))
-                depdict[(Symbol(ax), Symbol(mu))] .= dep
-                widdict[(Symbol(ax), Symbol(mu))] .= wid
-            else
-                depdict[(Symbol(ax), Symbol(mu))] = depall
-                widdict[(Symbol(ax), Symbol(mu))] = widall
-            end
-
-            # set bisector to zero if fixed
-            if fixed_bisector
-                wavdict[(Symbol(ax), Symbol(mu))] = zeros(size(wavall))
-            end
-
-            # get length
-            lengths[(Symbol(ax), Symbol(mu))] = size(wavall,2)
-        end
-    end
-    return SolarData(wavdict, bisdict, depdict, widdict, lengths, lp[1])
-end
-
-function calc_fixed_width(;λ::T1=5434.5232, M::T1=26.0, T::T1=5778.0, v_turb::T1=3.5e5) where T1<:AF
-    wid = width_thermal(λ=λ, M=M, T=T, v_turb=v_turb)
-    λs = collect(range(λ - 1.0, λ + 1.0, step=λ/7e5))
-    prof = gaussian_line.(λs, mid=λ, width=wid, depth=0.86)
-    depall, widall = calc_width_at_depth(λs, prof, len=100)
-    widall[1] = 0.01
-    return depall, widall
 end
