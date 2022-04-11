d3303a78797f7f154a13c3e1acb8413db00c73f7
Michael Palumbo <palumbomi95@gmail.com>
2022-04-11 15:01:09 -0400

    tweaks to preprocessing

--- src/preprocessing/preprocessing.jl
+++ src/preprocessing/preprocessing.jl
@@ -5,14 +5,35 @@ import PyPlot; plt = PyPlot; mpl = plt.matplotlib; plt.ioff()
 function fit_line_wings(wavs, spec; center=NaN, side="left")
     @assert !isnan(center)
 
+    # find flux at 25 and 80 percent depth
+    flux_lo = 0.10 * (1.0 - minimum(spec)) + minimum(spec)
+    flux_hi = 0.9 #0.80 * (1.0 - minimum(spec)) + minimum(spec)
+
     # cut out the middle
     botind = argmin(spec)
     if side == "left"
-        wavs_fit = wavs[15:botind-15]
-        spec_fit = spec[15:botind-15]
+        idx_lo = findfirst(x -> x .<= flux_lo, spec)
+        idx_hi = 1#findfirst(x -> x .<= flux_hi, spec)
+        wavs_fit = wavs[idx_hi:idx_lo]
+        spec_fit = spec[idx_hi:idx_lo]
+
+        # plt.plot(wavs, spec);
+        # plt.axvline(wavs[botind], c="k")
+        # plt.axhline(spec[idx_lo], c="k")
+        # plt.axhline(spec[idx_hi], c="k")
+        # plt.show()
     elseif side == "right"
-        wavs_fit = wavs[botind+15:end-15]
-        spec_fit = spec[botind+15:end-15]
+        idx_lo = findfirst(x -> x .>= flux_lo, spec[botind:end]) + botind
+        idx_hi = length(spec)#findfirst(x -> x .>= flux_hi, spec[botind:end]) + botind
+        idx_hi = length(spec)
+        wavs_fit = wavs[idx_lo:idx_hi]
+        spec_fit = spec[idx_lo:idx_hi]
+
+        # plt.clf();
+        # plt.plot(wavs, spec);
+        # plt.axvline(wavs[botind], c="k")
+        # plt.axhline(spec[idx_lo], c="k")
+        # plt.axhline(spec[idx_hi], c="k")
     end
 
     # perform the fit
@@ -21,7 +42,8 @@ function fit_line_wings(wavs, spec; center=NaN, side="left")
     return fit_voigt(wavs, fit.param)
 end
 
-function clean_line(wavs::AA{T,1}, spec::AA{T,1}; center::T=NaN, plot=false) where T<:Float64
+function clean_line(wavs::AA{T,1}, spec::AA{T,1}; center::T=NaN, plot=false,
+                    lwing_Δ_idx::Int=0, rwing_Δ_idx::Int=0) where T<:Float64
     # check that the length of arrays match
     @assert !isnan(center)
     @assert length(wavs) == length(spec)
@@ -37,73 +59,40 @@ function clean_line(wavs::AA{T,1}, spec::AA{T,1}; center::T=NaN, plot=false) whe
     center_wav = wavs[center_idx]
 
     # find indices for continuum on either side of line
-    c_inds = Peaks.argmaxima(spec, 10, strict=false)
-    c_idx_r = findfirst(wavs[c_inds] .> center_wav)
-    c_idx_l = c_idx_r - 1
+    if iszero(lwing_Δ_idx) && iszero(rwing_Δ_idx)
+        c_inds = Peaks.argmaxima(spec, 10, strict=false)
+        c_idx_r = findfirst(wavs[c_inds] .> center_wav)
+        c_idx_l = c_idx_r - 1
 
-    @show m_proms[m_inds_idx]
-    @show m_proms
-    # if m_proms[center_idx + 1]
+        @show c_inds[c_idx_l]
+        @show c_inds[c_idx_r]
 
-    # iterate until at end or within a standard deviation of max
-    # while abs.(maximum(spec) - spec[c_idx_r]) > 0.03 * std(spec)
-    #     if c_idx_r == length(c_inds)
-    #         break
-    #     else
-    #         c_idx_r += 1
-    #     end
-    # end
-
-    # while abs.(maximum(spec) - spec[c_idx_l]) > 0.03 * std(spec)
-    #     if c_idx_l == 1
-    #         break
-    #     else
-    #         c_idx_l -= 1
-    #     end
-    # end
-
-    # for i in wavs[m_inds]
-    #     plt.axvline(i, c="k")
-    # end
-    # plt.axvline(wavs[floor(Int64, m_left[m_inds_idx])])
-    # plt.axvline(wavs[ceil(Int64, m_right[m_inds_idx])])
-
-    # for i in wavs[c_inds]
-    #     plt.axvline(i, c="k")
-    # end
-
-    plt.axvline(wavs[c_inds][c_idx_l], c="k")
-    plt.axvline(wavs[c_inds][c_idx_r], c="k")
-    plt.axvline(center_wav, c="orange")
-    plt.plot(wavs, spec./maximum(spec)); plt.show()
-
-
-    # get better wing estimate
-    buffer = 1.0
-    lwingλ = center_wav - buffer
-    rwingλ = center_wav + buffer
-
-    # indices to narrow region of interest
-    lwavind = searchsortednearest(wavs, lwingλ)
-    rwavind = searchsortednearest(wavs, rwingλ)
-
-    # check the inds
-    if isnothing(lwavind)
-        lwavind = 1
+        plt.axvline(wavs[c_inds][c_idx_l], c="k")
+        plt.axvline(wavs[c_inds][c_idx_r], c="k")
+        plt.axvline(center_wav, c="orange")
+        plt.plot(wavs, spec./maximum(spec))
+        plt.show()
+        return nothing
     end
 
-    if isnothing(rwavind)
-        rwavind = length(wavs)
+    # calculate wing indices
+    lwing_idx = center_idx - lwing_Δ_idx
+    rwing_idx = center_idx + rwing_Δ_idx
+
+    if plot
+        plt.axvline(wavs[lwing_idx], c="k")
+        plt.axvline(wavs[rwing_idx], c="k")
+        plt.axvline(center_wav, ls="--", c="k")
+        plt.plot(wavs, spec./maximum(spec[lwing_idx:rwing_idx]))
     end
 
-    if plot; plt.plot(wavs, spec./maximum(spec[lwavind:rwavind])); end
-
-    # take view of spectrum isolated on line
-    newwavs = view(wavs, lwavind:rwavind)
-    newspec = view(spec, lwavind:rwavind)
+    # take view of spectrum isolated on line to fit
+    newwavs = view(wavs, lwing_idx:rwing_idx)
+    newspec = view(spec, lwing_idx:rwing_idx)
     newspec ./= maximum(newspec)
+    spec ./= maximum(newspec)
 
-    # find the wings
+    # find fluxes above 90% to replace with fit
     topint = 0.9
     botind = argmin(newspec)
     ind1 = findfirst(x -> x .<= topint, newspec[1:botind])
@@ -111,6 +100,7 @@ function clean_line(wavs::AA{T,1}, spec::AA{T,1}; center::T=NaN, plot=false) whe
 
     # abort if indices are weird
     if isnothing(ind1) || isnothing(ind2)
+        println("Indices are weird!!")
         wavs .= NaN
         spec .= NaN
         return wavs, spec
@@ -119,6 +109,12 @@ function clean_line(wavs::AA{T,1}, spec::AA{T,1}; center::T=NaN, plot=false) whe
     # get model for line wings
     lwing_flux = fit_line_wings(newwavs, newspec, center=center, side="left")
     rwing_flux = fit_line_wings(newwavs, newspec, center=center, side="right")
+
+    if plot
+        plt.plot(newwavs[1:ind1], lwing_flux[1:ind1], c="tab:orange")
+        plt.plot(newwavs[ind2:end], rwing_flux[ind2:end], c="tab:orange")
+        plt.show()
+    end
 
     # replace data wings with model wings
     newspec[1:ind1] .= lwing_flux[1:ind1]
@@ -129,12 +125,9 @@ function clean_line(wavs::AA{T,1}, spec::AA{T,1}; center::T=NaN, plot=false) whe
     vals = slope .* (newwavs .- newwavs[1]) .+ newspec[1]
     newspec ./= vals
 
-    # normalize it one last time
-    newspec ./= maximum(newspec)
-
     # now just set rest of spectrum to 1
-    spec[1:lwavind] .= 1.0
-    spec[rwavind:end] .= 1.0
+    spec[1:lwing_idx] .= 1.0
+    spec[rwing_idx:end] .= 1.0
     if plot; plt.plot(wavs, spec); plt.show(); end;
     return wavs, spec
 end
