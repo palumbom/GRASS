a77091efbdbac4bade71184b4aec0850a28b4ed2
Michael Palumbo <palumbomi95@gmail.com>
2022-04-19 09:28:18 -0400

    plot tweaks

--- figures2/bisector_intervals.jl
+++ figures2/bisector_intervals.jl
@@ -19,53 +19,67 @@ grassdir, plotdir, datadir = check_plot_dirs()
 
 # figure 1b -- input bisectors w/ variability
 function main()
-    # get input data
-    bisinfo = GRASS.SolarData()
+    # looping over inputs
+    dirs = filter(isdir, GRASS.soldir .* readdir(GRASS.soldir))
+    for dir in dirs
+        # figure out line name
+        line_name = splitpath(dir)[end]
+        line_title = replace(line_name, "_"=>" ")
 
-    # initialize plot objects
-    fig, ax1 = plt.subplots()
+        # get input data
+        bisinfo = GRASS.SolarData(dir=dir)
 
-    # loop and plot curves
-    keyz = [(:c, :mu10), (:w, :mu06), (:w, :mu03)]
-    labels = [L"\mu = 1.0", L"\mu = 0.6", L"\mu = 0.3"]
-    linestyles = ["-", "--", ":"]
-    for (i, key) in enumerate(keyz)
-        # find average and std
-        avg_bis = mean(bisinfo.bis[key], dims=2)
-        avg_wav = mean(bisinfo.wav[key], dims=2)
-        std_bis = std(bisinfo.bis[key], dims=2)
-        std_wav = std(bisinfo.wav[key], dims=2)
+        # initialize plot objects
+        fig, ax1 = plt.subplots()
 
-        # convert to doppler velocity
-        restwav = 5434.5232                         # angstroms
-        avg_wav = avg_wav ./ restwav .* GRASS.c_ms
-        std_wav = std_wav ./ restwav .* GRASS.c_ms
+        # loop and plot curves
+        keyz = [(:c, :mu10), (:w, :mu06), (:w, :mu03)]
+        labels = [L"\mu = 1.0", L"\mu = 0.6", L"\mu = 0.3"]
+        linestyles = ["-", "--", ":"]
+        for (i, key) in enumerate(keyz)
+            if !(key in keys(bisinfo.bis))
+                continue
+            end
 
-        # cut off top portion, where uncertainty is large
-        ind = findfirst(avg_bis .> 0.86)[1]
-        avg_bis = avg_bis[1:ind]
-        avg_wav = avg_wav[1:ind]
-        std_bis = std_bis[1:ind]
-        std_wav = std_wav[1:ind]
+            # find average and std
+            avg_bis = mean(bisinfo.bis[key], dims=2)
+            avg_wav = mean(bisinfo.wav[key], dims=2)
+            std_bis = std(bisinfo.bis[key], dims=2)
+            std_wav = std(bisinfo.wav[key], dims=2)
 
-        # fix dimensions
-        y = reshape(avg_bis, length(avg_bis))
-        x1 = reshape(avg_wav .+ std_wav, length(avg_bis))
-        x2 = reshape(avg_wav .- std_wav, length(avg_bis))
+            # convert to doppler velocity
+            restwav = 5434.5232                         # angstroms
+            avg_wav = avg_wav ./ restwav .* GRASS.c_ms
+            std_wav = std_wav ./ restwav .* GRASS.c_ms
 
-        # plot the curve
-        ax1.fill_betweenx(y, x1, x2, color="C"*string(i-1), alpha=0.5)
-        ax1.plot(avg_wav, avg_bis, ls=linestyles[i], color="C"*string(i-1), label=labels[i])
+            # cut off top portion, where uncertainty is large
+            ind = findfirst(avg_bis .> 0.86)[1]
+            avg_bis = avg_bis[1:ind]
+            avg_wav = avg_wav[1:ind]
+            std_bis = std_bis[1:ind]
+            std_wav = std_wav[1:ind]
+
+            # fix dimensions
+            y = reshape(avg_bis, length(avg_bis))
+            x1 = reshape(avg_wav .+ std_wav, length(avg_bis))
+            x2 = reshape(avg_wav .- std_wav, length(avg_bis))
+
+            # plot the curve
+            ax1.fill_betweenx(y, x1, x2, color="C"*string(i-1), alpha=0.5)
+            ax1.plot(avg_wav, avg_bis, ls=linestyles[i], color="C"*string(i-1), label=labels[i])
+        end
+
+        # set axes labels and save the figure
+        ax1.legend(loc="upper right")
+        ax1.set_xlim(-500, 500)
+        ax1.set_ylim()
+        ax1.set_title(latexstring(line_title))
+        ax1.set_xlabel(L"{\rm Doppler\ Velocity\ (ms}^{-1} {\rm )}")
+        ax1.set_ylabel(L"{\rm Normalized\ Intensity}")
+        fig.savefig(plotdir * "new_stuff/" * line_name * "_interval.pdf")
+        plt.clf(); plt.close()
+        println(">>> Figure written to: " * plotdir * "new_stuff/" * line_name * "_interval.pdf")
     end
-
-    # set axes labels and save the figure
-    ax1.legend(loc="upper right")
-    ax1.set_ylim()
-    ax1.set_xlabel(L"{\rm Doppler\ Velocity\ (ms}^{-1} {\rm )}")
-    ax1.set_ylabel(L"{\rm Normalized\ Intensity}")
-    fig.savefig(plotdir * "fig1b.pdf")
-    plt.clf(); plt.close()
-    println(">>> Figure written to: " * plotdir * "fig1b.pdf")
     return nothing
 end
 
