438c380bb66a47971861800d925c2b6be82d1219
Michael Palumbo <palumbomi95@gmail.com>
2022-08-11 14:54:21 -0400

    got line params writing working

--- src/iag_utils.jl
+++ src/iag_utils.jl
@@ -2,7 +2,7 @@ using HTTP
 using GZip
 using CSV
 using DataFrames
-using EchelleCCFs: λ_air_to_vac
+using EchelleCCFs: λ_air_to_vac, λ_vac_to_air
 
 function download_iag()
     println(">>> Downloading IAG atlas...")
@@ -12,7 +12,7 @@ function download_iag()
     return nothing
 end
 
-function read_iag(; airwav::Float64=5434.5)
+function read_iag(; isolate::Bool=false, airwav::Float64=5434.5)
     # download the IAG atlas
     file = GRASS.moddir * "input_data/spvis.dat.gz"
     if !isfile(file)
@@ -32,8 +32,12 @@ function read_iag(; airwav::Float64=5434.5)
     reverse!(iag.nflux)
 
     # isolate region around line
-    vacwav = λ_air_to_vac(airwav)
-    ind1 = findfirst(x -> x .> vacwav-1.0, wavs)
-    ind2 = findfirst(x -> x .> vacwav+1.0, wavs)
-    return λ_vac_to_air.(view(wavs,ind1:ind2)), view(iag.nflux, ind1:ind2)
+    if isolate
+        vacwav = λ_air_to_vac(airwav)
+        ind1 = findfirst(x -> x .> vacwav-1.0, wavs)
+        ind2 = findfirst(x -> x .> vacwav+1.0, wavs[ind1:end]) + ind1
+        return λ_vac_to_air.(view(wavs,ind1:ind2)), view(iag.nflux, ind1:ind2)
+    else
+        return λ_vac_to_air.(wavs), iag.nflux
+    end
 end
