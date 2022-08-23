728e34144e8459c2866282fa27e36803fa8f923d
Michael Palumbo <palumbomi95@gmail.com>
2022-08-23 15:01:06 -0400

    fixed bug in trimming relating to width scaling

--- src/trim.jl
+++ src/trim.jl
@@ -1,28 +1,26 @@
-function trim_bisector!(depth::T, bist::AA{T,1}, intt::AA{T,1}, widt::AA{T,1}) where T<:AF
+function trim_bisector!(depth::T, bist::AA{T,1}, intt::AA{T,1}) where T<:AF
     # choose method depending if synth line is deeper than input
     if (one(T) - depth) > minimum(bist)
-        trim_bisector_chop!(depth, bist, intt, widt)
+        trim_bisector_chop!(depth, bist, intt)
     else
-        trim_bisector_scale!(depth, bist, intt, widt)
+        trim_bisector_scale!(depth, bist, intt)
     end
     return nothing
 end
 
-function trim_bisector_chop!(depth::T, bist::AA{T,1}, intt::AA{T,1}, widt::AA{T,1}) where T<:AF
+function trim_bisector_chop!(depth::T, bist::AA{T,1}, intt::AA{T,1}) where T<:AF
     # create interpolators
     itp1 = linear_interp(intt, bist)
-    itp2 = linear_interp(intt, widt)
 
     # get new grid of depths, interpolate the data, and return
-    # TODO check if there is a memory clash with overwriting intt?
+    # TODO figure out memory with new_int
     new_int = range((one(T) - depth), one(T), length=length(intt))
     bist .= itp1.(new_int)
-    widt .= itp2.(new_int)
     intt .= new_int
     return nothing
 end
 
-function trim_bisector_scale!(depth::T, bist::AA{T,1}, intt::AA{T,1}, widt::AA{T,1}) where T<:AF
+function trim_bisector_scale!(depth::T, bist::AA{T,1}, intt::AA{T,1}) where T<:AF
     # get new grid of depths, effectively scaling the width and bisector data
     intt .= range((one(T) - depth), one(T), length=length(intt))
     return nothing
