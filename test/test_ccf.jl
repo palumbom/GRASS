897292c3ca490e7f72db09672f605f0740d5f994
Michael Palumbo <palumbomi95@gmail.com>
2022-08-24 10:24:53 -0400

    fixed test syntax

--- test/test_ccf.jl
+++ test/test_ccf.jl
@@ -5,6 +5,7 @@ import EchelleCCFs
 @testset "CCF" begin
 
 @testset "Testing function definitions" begin
+    @test isdefined(GRASS, :fit_voigt)
     @test isdefined(GRASS, :calc_ccf)
     @test isdefined(GRASS, :calc_rvs_from_ccf)
     @test isdefined(EchelleCCFs, :calc_doppler_factor)
@@ -21,12 +22,12 @@ end
 
     # generate spectra
     res = 7e5
-    buff = 0.75
+    buff = 1.25
     Δlnλ = (1.0 / res)
     wavs = exp.(range(log(mid-buff), log(mid+buff), step=Δlnλ))
     flux = zeros(length(wavs), length(λvs))
     for i in eachindex(λvs)
-        flux[:,i] = GRASS.fit_voigt(wavs, [-0.15, λvs[i], 0.04, 0.045, 1.0])
+        flux[:,i] = GRASS.fit_voigt(wavs, [0.5, λvs[i], 0.04, 0.045])
     end
 
     # calculate ccf and measure velocity
@@ -38,7 +39,7 @@ end
 
     # test that the residuals are within 10 cm/s
     delta = vel .- rvs
-    @test maximum(delta) < 0.1 # 0.1 m/s = 10 cm/s
+    @test maximum(delta) < 0.01 # 0.0 m/s = 1 cm/s
 end
 
 end
