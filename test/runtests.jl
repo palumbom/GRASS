e5bda4f030f966fc303f0671bd333de6a7e10504
Michael Palumbo <palumbomi95@gmail.com>
2022-08-24 09:22:08 -0400

    deps for julia 1.8

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ using Test
 include("test_geometry.jl")
 include("test_physics.jl")
 include("test_ccf.jl")
-include("test_interpolations.jl")   # TODO: obsoleted?
+include("test_interpolations.jl")
 include("test_input.jl")
 include("test_synthesis.jl")
 
