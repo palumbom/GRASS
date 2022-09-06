044315e7894531f6759196a1c9bcbffab13baacd
Michael Palumbo <palumbomi95@gmail.com>
2022-09-06 13:55:46 -0400

    voigt fitting

--- src/preprocessing/voigt.jl
+++ src/preprocessing/voigt.jl
@@ -1,12 +1,12 @@
 import SpecialFunctions.erfcx
 
 function gaussian(x, amp, mu, sigma)
-    return 1.0 - amp * exp(-0.5 * ((x - mu)/sigma)^2)
+    return amp * exp(-0.5 * ((x - mu)/sigma)^2)
 end
 
 
 function lorentzian(x, amp, mu, gamma)
-    return 1.0 - amp * (gamma^2 / ((x - mu)^2 + gamma^2))
+    return amp * (gamma^2 / ((x - mu)^2 + gamma^2))
 end
 
 function faddeeva(x::Complex{T}) where T<:AF
@@ -20,9 +20,9 @@ function voigt(x, amp, mu, sigma, gamma)
         return gaussian(x, amp, mu, sigma)
     else
         z = ((x - mu) + im * gamma) / (sqrt(2) * sigma)
-        return 1.0 - amp * real(faddeeva(z)) / (sigma * sqrt(2π))
+        return amp * real(faddeeva(z)) / (sigma * sqrt(2π))
     end
     return nothing
 end
 
-@. fit_voigt(x, p) = voigt(x, p[1], p[2], p[3], p[4])
+@. fit_voigt(x, p) = exp(-voigt(x, p[1], p[2], p[3], p[4]))
