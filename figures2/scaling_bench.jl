5a830b6817eb2598c07c201f6a6cb20289c49dd6
Michael Palumbo <palumbomi95@gmail.com>
2023-01-18 11:08:40 -0500

    better scaling bench + writeout

--- figures2/scaling_bench.jl
+++ figures2/scaling_bench.jl
@@ -4,105 +4,141 @@ using Profile
 using BenchmarkTools
 using CUDA
 using GRASS
+using JLD2
+using FileIO
 
 using LaTeXStrings
 import PyPlot; plt = PyPlot; mpl = plt.matplotlib; plt.ioff()
-# mpl.style.use("my.mplstyle")
+mpl.style.use(GRASS.moddir * "figures1/fig.mplstyle")
 
-# define some functions
+# parse args + get directories
+run, plot = parse_args(ARGS)
 grassdir, plotdir, datadir = check_plot_dirs()
 
-@assert CUDA.functional()
+function precompile_with_spectra_test()
+    N = 132
+    Nt = 50
+    lines = [5434.5]
+    depths = [0.5]
+    geffs = [0.0]
+    templates = ["FeI_5434"]
+    variability = repeat([true], length(lines))
+    resolution = 7e5
 
-# benchmark on cpu
-function benchmark_cpu(lines::AbstractArray{Float64,1}, depths::AbstractArray{Float64,1})
-    templates = repeat(["FeI_5434"], length(lines))
-    resolution = 700000.0
-    spec = SpecParams(lines=lines, depths=depths, templates=templates, resolution=resolution)
-    disk = DiskParams(N=132, Nt=50)
+    disk = DiskParams(N=N, Nt=Nt)
+    spec = SpecParams(lines=lines, depths=depths, variability=variability,
+                       geffs=geffs, templates=templates, resolution=resolution)
 
-    # synthesize it
+    lambdas1, outspec = synthesize_spectra(spec, disk, seed_rng=true, use_gpu=true)
+    return nothing
+end
+
+function benchmark_cpu(spec::SpecParams, disk::DiskParams)
     lambdas1, outspec1 = synthesize_spectra(spec, disk, verbose=false)
     return nothing
 end
 
-# benchmark on gpu
-function benchmark_gpu(lines::AbstractArray{Float64,1}, depths::AbstractArray{Float64,1})
-    templates = repeat(["FeI_5434"], length(lines))
-    resolution = 700000.0
-    spec = SpecParams(lines=lines, depths=depths, templates=templates, resolution=resolution)
-    disk = DiskParams(N=132, Nt=50)
-
-    # synthesize it
+function benchmark_gpu(spec::SpecParams, disk::DiskParams)
     lambdas1, outspec1 = synthesize_spectra(spec, disk, verbose=false, use_gpu=true)
     return nothing
 end
 
-# line parameters
-nlines = 24
-lines = range(5434.5, step=5.0, length=nlines)
-depths = repeat([0.75], length(lines))
-
-# initial for loop to calculate number of reoslution elements per spectrum
-n_res = similar(lines)
-n_lam = similar(lines)
-for i in eachindex(lines)
-    # calculate coverage
-    lines1 = lines[1:i]
-    coverage = (minimum(lines1) - 0.75, maximum(lines1) + 0.75)
-
-    # generate Delta ln lambda
-    Δlnλ = (1.0 / 700000.0)
-    lnλs = range(log(coverage[1]), log(coverage[2]), step=Δlnλ)
-    lambdas = exp.(lnλs)
-    n_res[i] = length(lambdas)
-    n_lam[i] = lambdas[end] - lambdas[1]
-end
-
 # benchmarking wrapper function
 function bmark_everything(b_cpu, b_gpu, lines, depths; max_cpu=8)
-    # initial run to precompile things
-    spec1 = SpecParams(lines=[5434.5], depths=[0.75], templates=["FeI_5434"], resolution=7e5)
-    disk1 = DiskParams(N=132, Nt=10)
-    lambdas1, outspec1 = synthesize_spectra(spec1, disk1, seed_rng=false, use_gpu=false)
-    lambdas1, outspec1 = synthesize_spectra(spec1, disk1, seed_rng=false, use_gpu=true)
-
     for i in eachindex(lines)
-        @printf("Benchmarking %s of %s\n", i, length(lines))
+        @printf(">>> Benchmarking %s of %s\n", i, length(lines))
 
+        # number of loops for GPU
+        n_gpu_loops = 8
+
+        # get lines and depths
         lines_i = lines[1:i]
         depths_i = depths[1:i]
+        templates_i = repeat(["FeI_5434"], length(lines_i))
+        resolution_i = 7e5
 
+        # create spec, disk instances
+        spec = SpecParams(lines=lines_i, depths=depths_i, templates=templates_i, resolution=resolution_i)
+        disk = DiskParams(N=132, Nt=50)
+
+        # CPU bench loop
         if i <= max_cpu
-            @printf("Performing CPU bench (N=132, Nt=50): ")
+            @printf("\t>>> Performing CPU bench (N=132, Nt=50): ")
             Profile.clear_malloc_data()
-            b_cpu[i] = @belapsed benchmark_cpu($lines_i, $depths_i)
+            b_cpu[i] = @belapsed benchmark_cpu($spec, $disk)
+            # b_cpu[i] = 0.0
             println()
         end
 
-        @printf("Performing GPU bench (N=132, Nt=50): ")
-        Profile.clear_malloc_data()
-        b_gpu[i] = @belapsed benchmark_gpu($lines_i, $depths_i)
+        # GPU bench loop
+        @printf("\t>>> Performing GPU bench (N=132, Nt=50): ")
+        for j in 1:n_gpu_loops
+            Profile.clear_malloc_data()
+            b_gpu[i] += @belapsed benchmark_gpu($spec, $disk)
+        end
+        b_gpu[i] /= n_gpu_loops
         println()
     end
     return nothing
 end
 
-# allocate memory for benchmark results and run it
-num = 8
-max_cpu = minimum([num, length(lines)])
-b_cpu = similar(lines)
-b_gpu = similar(lines)
-bmark_everything(b_cpu, b_gpu, lines, depths, max_cpu=max_cpu)
+function main()
+    # make sure its precompiled
+    println(">>> Doing precompilation synthesis...")
+    precompile_with_spectra_test()
 
-# function for plotting since it dies in global??
-function plot_it()
+    # line parameters
+    nlines = 24
+    lines = range(5434.5, step=5.0, length=nlines)
+    depths = repeat([0.75], length(lines))
+
+    # calculate number of resolution elements per spectrum
+    n_res = similar(lines)
+    n_lam = similar(lines)
+    for i in eachindex(lines)
+        # calculate wavelength coverage
+        lines1 = lines[1:i]
+        coverage = (minimum(lines1) - 0.75, maximum(lines1) + 0.75)
+
+        # generate Delta ln lambda
+        Δlnλ = (1.0 / 7e5)
+        lnλs = range(log(coverage[1]), log(coverage[2]), step=Δlnλ)
+        lambdas = exp.(lnλs)
+        n_res[i] = length(lambdas)
+        n_lam[i] = last(lambdas) - first(lambdas)
+    end
+
+    # allocate memory for benchmark results and run it
+    num = 8
+    max_cpu = minimum([num, length(lines)])
+    b_cpu = similar(lines)
+    b_gpu = similar(lines)
+    bmark_everything(b_cpu, b_gpu, lines, depths, max_cpu=max_cpu)
+
+    # write info to disk
+    outfile = datadir * "scaling_benchmark.jld2"
+    save(outfile,
+         "nlines", nlines,
+         "max_cpu", max_cpu,
+         "n_res", n_res,
+         "n_lam", n_lam,
+         "b_cpu", b_cpu,
+         "b_gpu", b_gpu)
+    return nothing
+end
+
+if run
+    main()
+end
+
+    # function for plotting since it dies in global??
+if plot
     fig, ax1 = plt.subplots()
 
     # plot on ax1
-    ax1.plot(n_res[1:max_cpu], b_cpu[1:max_cpu], c="k", label="CPU")
+    ax1.plot(n_res[1:max_cpu], b_cpu[1:max_cpu], c="k", label=L"{\rm CPU}")
     ax1.scatter(n_res[1:max_cpu], b_cpu[1:max_cpu], c="k")
-    ax1.plot(n_res, b_gpu, c="tab:blue", label="GPU")
+    ax1.plot(n_res, b_gpu, c="tab:blue", label=L"{\rm GPU}")
     ax1.scatter(n_res, b_gpu, c="tab:blue")
 
     # plot on twin axis
@@ -111,14 +147,12 @@ function plot_it()
     ax2.scatter(n_lam[1:max_cpu], b_cpu[1:max_cpu], c="k")
     ax2.plot(n_lam, b_gpu, c="tab:blue")
     ax2.scatter(n_lam, b_gpu, c="tab:blue")
+    ax2.grid(false)
 
-    ax1.set_xlabel("Number of spectral resolution elements")
-    ax1.set_ylabel("Simulation time (s)")
-    ax2.set_xlabel("Width of spectrum (A)")
+    ax1.set_xlabel(L"{\rm \#\ of\ res.\ elements}")
+    ax1.set_ylabel(L"{\rm Simulation\ time\ (s)}")
+    ax2.set_xlabel(L"{\rm Width\ of\ spectrum\ (\AA)}")
     ax1.legend()
     fig.savefig(plotdir * "scaling_bench.pdf")
-    return nothing
+    plt.clf(); plt.close()
 end
-
-plot_it()
-
