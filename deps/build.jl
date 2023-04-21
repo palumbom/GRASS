ed469eb47a3bf42e0e89245d6600793dfff9d7b7
Michael Palumbo <palumbomi95@gmail.com>
2023-04-21 14:38:39 -0400

    build step should be working now, tests pass locally

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,13 @@
+using Tar, CodecZlib
+
 download_url = "https://zenodo.org/record/7853524/files/input_data.tar.gz?download=1"
-download_loc = joinpath("..", "input_data")
+download_loc = joinpath("..", "input_data.tar.gz")
 
 download(download_url, download_loc)
+
+open(GzipDecompressorStream, download_loc) do io
+    Tar.extract(io, joinpath("..", "tmp"))
+end
+
+mv(joinpath("..", "tmp", "input_data"), joinpath("..", "input_data"), force=true)
+rm(joinpath("..", "tmp"), force=true)
