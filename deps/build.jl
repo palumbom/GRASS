5dac98670d9c343e35b17aec7eeb3b29e29969dc
Michael Palumbo <palumbomi95@gmail.com>
2023-04-25 13:41:32 -0400

    build step

--- deps/build.jl
+++ deps/build.jl
@@ -8,11 +8,15 @@ soldir = abspath(joinpath(datdir, "input/"))
 @assert isdir(datdir)
 if !isdir(soldir)
     mkdir(soldir)
+elseif !isempty(soldir)
+    rm(soldir, recursive=true, force=true)
+    mkdir(soldir)
 end
 
 download_url = "https://g-50c10.ffdaa9.e229.data.globus.org/GRASS_Input/input.tar.gz"
 download_loc = abspath(joinpath(datdir, "input.tar.gz"))
 
 download(download_url, download_loc)
+
 
 Tar.extract(download_loc, soldir)
