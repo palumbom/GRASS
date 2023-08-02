d04245ee29bf54da79d301db93f9ad4bb320a1a4
Michael Palumbo <palumbomi95@gmail.com>
2023-08-01 21:56:47 -0400

    reduced memory overhead

--- src/three_dim.jl
+++ src/three_dim.jl
@@ -38,8 +38,8 @@ M = [1.0 0.0 0.0;
      0.0 -sin(iₛ) cos(iₛ)]
 
 # set up lat long grid edges
-ϕs = deg2rad.(range(-90.0, 90.0, length=91))
-θs = deg2rad.(range(0.0, 360.0, length=46))
+ϕs = deg2rad.(range(-90.0, 90.0, length=181))
+θs = deg2rad.(range(0.0, 360.0, length=361))
 
 # get grid centers
 θc = (θs[2:end] + θs[1:end-1])/2.0
@@ -49,12 +49,6 @@ M = [1.0 0.0 0.0;
 dθ = diff(θs)
 dϕ = diff(ϕs)
 
-# get cartesian coordinates of surface elements
-xyz = get_xyz_for_surface.(ρₛ, ϕc, θc')
-xs = getindex.(xyz, 1)
-ys = getindex.(xyz, 2)
-zs = getindex.(xyz, 3)
-
 # pole vector in star frame
 pole_vec = [0.0, 0.0, ρₛ]
 
@@ -62,19 +56,17 @@ pole_vec = [0.0, 0.0, ρₛ]
 obs = [0.0, 220.0, 0.0]
 
 # get velocity vector in star frame
-xyz_new = similar(xyz)
-vel_new = similar(xyz)
+μs = zeros(length(ϕc), length(θc))
+vs = similar(μs)
+dA_proj = similar(vs)
 
-μs = similar(xs)
-vel_proj = similar(xs)
+for i in 1:length(ϕc)
+    for j in 1:length(θc)
+        # get cartesian coords
+        xyz = get_xyz_for_surface(ρₛ, ϕc[i], θc[j])
 
-dA = zeros(length(ϕc), length(θc))
-dA_proj = similar(dA)
-
-for i in 1:size(xs, 1)
-    for j in 1:size(xs, 2)
         # get vector pointing from star origin to annulus height
-        ann_vec = xyz[i,j] .- [0.0, 0.0, last(xyz[i,j])]
+        ann_vec = xyz .- [0.0, 0.0, last(xyz)]
 
         # take cross product to get vector in direction of surface rotation
         vel = cross(ann_vec, pole_vec)
@@ -82,55 +74,51 @@ for i in 1:size(xs, 1)
         vel *= differential_rotation(ρₛ, ϕs[i])
 
         # rotate by stellar inclination
-        xyz_new[i,j] = M * xyz[i,j]
-        vel_new[i,j] = M * vel
+        xyz_new = M * xyz
+        vel_new = M * vel
 
         # get mu
-        μs[i,j] = dot(obs, xyz_new[i,j]) / (norm(obs) * norm(xyz_new[i,j]))
+        μs[i,j] = dot(obs, xyz_new) / (norm(obs) * norm(xyz_new))
 
         # find get vector from observer to surface patch
-        v_obs_surf = xyz_new[i,j] .- obs
-        angle = dot(v_obs_surf, vel_new[i,j]) / (norm(v_obs_surf) * norm(vel_new[i,j]))
+        v_obs_surf = xyz_new .- obs
+        angle = dot(v_obs_surf, vel_new) / (norm(v_obs_surf) * norm(vel_new))
 
         # project velocities along line of sight
-        vel_proj[i,j] = norm(vel_new[i,j]) * angle
+        vs[i,j] = norm(vel_new) * angle
 
         # get surface element area
-        dA[i,j] = ρₛ^2.0 * sin(π/2.0 - ϕc[i]) * dϕ[i] * dθ[j]
+        dA = ρₛ^2.0 * sin(π/2.0 - ϕc[i]) * dϕ[i] * dθ[j]
 
         # get project surface element area
-        dA_proj[i,j] = dA[i,j] * abs(dot(v_obs_surf, xyz_new[i,j]))
+        dA_proj[i,j] = dA * abs(dot(v_obs_surf, xyz_new))
     end
 end
 
 # parse out coords
+xyz_new = [M * i for i in get_xyz_for_surface.(ρₛ, ϕc, θc')]
 xs_new = getindex.(xyz_new, 1)
 ys_new = getindex.(xyz_new, 2)
 zs_new = getindex.(xyz_new, 3)
-
-vx_new = getindex.(vel_new./norm.(vel_new), 1)
-vy_new = getindex.(vel_new./norm.(vel_new), 2)
-vz_new = getindex.(vel_new./norm.(vel_new), 3)
 
 # now plot the projection
 idx = μs .< 0.0
 μs_plot = copy(μs)
 μs_plot[idx] .= NaN
 
-vel_plot = copy(vel_proj)
+vel_plot = copy(vs)
 vel_plot[idx] .= NaN
 
 # plt.pcolormesh(xs_new, zs_new, rad2deg.(acos.(μs_plot)))
-# plt.pcolormesh(xs_new, zs_new, vel_proj, cmap="seismic", vmin=-2000, vmax=2000, edgecolors="k")
-plt.pcolormesh(xs_new, zs_new, dA_proj)#, edgecolors="k")
+# plt.pcolormesh(xs_new, zs_new, vs, cmap="seismic", vmin=-2000, vmax=2000, edgecolors="k")
+plt.pcolormesh(xs_new, zs_new, dA_proj./sum(dA_proj))#, edgecolors="k")
 plt.colorbar()
 plt.show()
 
 # cmap = mpl.cm.ScalarMappable(cmap="viridis")
-# clrs = cmap.to_rgba(dA)
+# clrs = cmap.to_rgba(dA_proj)
 # ax = plt.figure().add_subplot(projection="3d")
 # ax.plot_surface(xs_new, ys_new, zs_new, facecolors=clrs)
-# # ax.quiver(xs_new, ys_new, zs_new, vx_new, vy_new, vz_new)
 # plt.xlabel("x")
 # plt.ylabel("y")
 # plt.zlabel("z")
