6bd32633e995ffbf1513f73112988dc84ed3fdb3
Michael Palumbo <palumbomi95@gmail.com>
2023-08-02 13:30:48 -0400

    removed relic funcs

--- src/stargeometry.jl
+++ src/stargeometry.jl
@@ -1,9 +1,6 @@
 # set discrete values of mu for input observations
 const disc_ax = [:n, :e, :s, :w, :c]
 
-# make_grid(N::Integer) = range(-1.0, 1.0, length=N)
-# make_grid(;N::Integer=256) = make_grid(N)
-
 function make_grid(N::Integer)
     # create grid edges
     ϕe = range(deg2rad(-90.0), deg2rad(90.0), length=N)
@@ -34,23 +31,6 @@ function calc_projected_area_element(ϕc::T, θc::T, disk::DiskParams{T}) where 
     return dA * abs(dot(O⃗_surf, xyz))
 end
 
-
-function calc_dist2(x1::T, y1::T, x2::T, y2::T) where T<:AF
-    return (x1 - x2)^2 + (y1 - y2)^2
-end
-
-function calc_dist2(t1::Tuple{T,T}, t2::Tuple{T,T}) where T<:AF
-    return calc_dist2(t1..., t2...)
-end
-
-function calc_r2(x::T,y::T) where T<:AF
-    return x*x + y*y
-end
-
-function calc_r2(t::Tuple{T,T}) where T<:AF
-    return calc_r2(t[1], t[2])
-end
-
 function sphere_to_cart(ρ::T, ϕ::T, θ::T) where T
     # compute trig quantitites
     sinϕ = sin(ϕ)
@@ -71,80 +51,6 @@ function calc_mu(ϕ::T, θ::T; R_θ::AA{T,2}=Matrix(1.0I,3,3), O⃗::AA{T,1}=[0.
     xyz .= R_θ * xyz
     return dot(O⃗, xyz) / (norm(O⃗) * norm(xyz))
 end
-
-# Calculate mu for each position on a grid
-function mu_map(grid::AA{T,1}) where T<:AF
-    return calc_mu.((x,y) for x in grid, y in grid)
-end
-
-function mu_map(N::Integer)
-    return mu_map(make_grid(N))
-end
-
-function calc_norm_term(μ::T, N::Integer, u1::T, u2::T) where T<:AF
-    return quad_limb_darkening(μ,u1,u2) * π / (2.0 * N^2)
-end
-
-function calc_norm_term(x::T, y::T, N::Integer, u1::T, u2::T) where T<:AF
-    return calc_norm_term(calc_mu(x,y), N, u1, u2)
-end
-
-function calc_norm_term(x::T, y::T, disk::DiskParams) where T<:AF
-    return calc_norm_term(calc_mu(x,y), disk.N, disk.u1, disk.u2)
-end
-
-
-function calc_norm_terms(N::Int, u1::T, u2::T; nsubgrid::Int=256) where T<:AF
-    # get grids
-    len = Int(N/2)
-    grid = Iterators.product(range(0.0, 1.0, length=len),
-                             range(0.0, 1.0, length=len))
-    grid_range = range(0.0, 1.0, length=len)
-    grid_edges = get_grid_edges(grid_range)
-
-    # allocate memory for norm_terms
-    norm_terms = zeros(N, N)
-    norm_terms_quadrant = zeros(size(grid))
-
-    # calculate distances
-    dist2 = map(z -> calc_dist2(z, (0.0, 0.0)), grid)
-
-    # find edge pixels
-    is_edge = (dist2 .>= (1.0 - step(grid_range))) .&& (dist2 .<= 1.0 + step(grid_range))
-    is_not_edge = (.!is_edge) .&& (dist2 .< 1.0)
-
-    # calculate norm terms on not_edge pixels
-    for idx in findall(is_not_edge) #CartesianIndices(norm_terms)
-        i = idx[1]; j= idx[2]
-        norm_terms_quadrant[idx] = quad_limb_darkening.(grid_range[i], grid_range[j], u1, u2)
-    end
-
-    # calculate norm terms for edge pixels
-    for idx in findall(is_edge)
-        i = idx[1]; j= idx[2]
-
-        # make subgrid
-        xrange = range(grid_edges[i], grid_edges[i+1], length=nsubgrid)
-        yrange = range(grid_edges[j], grid_edges[j+1], length=nsubgrid)
-        subgrid = Iterators.product(xrange, yrange)
-
-        # calculate norm terms on subgrid
-        sublimbdarks = quad_limb_darkening.(calc_mu.(subgrid), u1, u2)
-        norm_terms_quadrant[idx] = mean(sublimbdarks)
-    end
-
-    # now copy quadrants over
-    norm_terms[len+1:end, len+1:end] .= norm_terms_quadrant
-    norm_terms[1:len, len+1:end] .= reverse!(norm_terms_quadrant, dims=(1))
-    norm_terms[len+1:end, 1:len] .= reverse!(norm_terms_quadrant, dims=(1,2))
-    norm_terms[1:len, 1:len] .= reverse!(norm_terms_quadrant, dims=(1))
-    return norm_terms ./ sum(norm_terms)
-end
-
-function calc_norm_terms(disk::DiskParams; nsubgrid::Int=256)
-    return calc_norm_terms(disk.N, disk.u1, disk.u2, nsubgrid=nsubgrid)
-end
-
 
 # Find the nearest axis to a given point on a grid
 function find_nearest_ax(x::T, y::T) where T<:AF
