3703c9519e05fe65f936fc3bba8a1f8cf38a7fdb
Michael Palumbo <palumbomi95@gmail.com>
2023-09-14 16:36:57 -0400

    forgot this one

---
+++ src/gpu/gpu_state_vectors.jl
@@ -0,0 +1,12 @@
+function calc_state_vector_gpu!()
+
+
+    return nothing
+end
+
+
+function calc_state_vector!(ros_allocs::RossiterAllocsGPU{T1}, body::Planet{T2}) where {T1<:AF, T2<:AF}
+
+
+    return nothing
+end
