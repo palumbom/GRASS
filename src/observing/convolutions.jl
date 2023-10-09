47adf3abf5243a05067dd8df33dc732bd86824a5
Michael Palumbo <palumbomi95@gmail.com>
2023-10-09 11:32:13 -0400

    convolution time loop function

--- src/observing/convolutions.jl
+++ src/observing/convolutions.jl
@@ -147,3 +147,40 @@ function convolve_gauss(xs::AA{T,1}, ys::AA{T,1}; new_res::T=1.17e5,
     ys_out = rebin_spectrum(xs, new_ys, xs_out)
     return xs_out, ys_out
 end
+
+function convolve_gauss(xs::AA{T,1}, ys::AA{T,2}; new_res::T=1.17e5,
+                        oversampling::T=1.0) where T<:AbstractFloat
+    # get kernel
+    # TODO: wavelength dependent kernel width???
+    σ = mean(xs) / new_res / 2.354
+    g(x, n) = (one(T)/(σ * sqrt(2.0 * π))) * exp(-0.5 * ((x - n)/σ)^2)
+    kernel = g.(xs, xs[Int(round(length(xs)/2))])
+
+    # create vector to hold padded signal
+    signal = zeros(200 + size(ys, 1))
+
+    # get wavelength grid at lower resolution
+    Δlnλ = 1.0 / new_res
+    lnλs = range(log(first(xs)), log(last(xs)), step=Δlnλ/oversampling)
+    xs_out = exp.(lnλs)
+
+    # allocate array for output spectrum
+    ys_out = zeros(length(xs_out), size(ys, 2))
+
+    # loop over times
+    for t in 1:size(ys,2)
+        # fill signal vector
+        signal[1:100] .= first(ys[:,t])
+        signal[101:end-100] .= ys[:,t]
+        signal[end-100:end] .= last(ys[:,t])
+
+        # perform the convolution
+        signal .= imfilter(signal, reflect(centered(kernel./maximum(kernel))), Fill(0))
+        signal ./= maximum(signal[101:end-100])
+
+        # rebin the convolved spectrum
+        ys_out[:,t] .= rebin_spectrum(xs, view(signal, 101:length(signal)-100), xs_out)
+    end
+
+    return xs_out, ys_out
+end
