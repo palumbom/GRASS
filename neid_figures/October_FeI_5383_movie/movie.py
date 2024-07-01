6c1d90a89d3173d12da67b27e429716e1734918a
elizabethg60 <nobody@nowhere.org>
2024-06-30 20:37:53 -0400

    neid lines sorted

---
+++ neid_figures/October_FeI_5383_movie/movie.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageDraw
+import glob
+
+frames = []
+imgs = []
+for i in range(1,158):
+    imgs.append("timestamp_{}.png".format(i))
+
+for i in imgs:
+    new_frame = Image.open(i)
+    frames.append(new_frame)
+
+frames[0].save('movie.gif', format='GIF', append_images=frames[1:], save_all=True, duration = 280, Loop=0)
